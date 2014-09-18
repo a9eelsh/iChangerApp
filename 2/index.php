cd86ba0092a099bd1816f3d228485e46b61d80e1
Rosco1502 <thelocken@gmail.com>
2014-09-18 02:43:19 +0000

    Fixed PC Redirect

--- 2/index.php
+++ 2/index.php
@@ -8,11 +8,11 @@ if ( strpos($ua,"iphone") || strpos($ua,"ipad") ) {
    } else if ( substr_count($ua, '/') === 3 ) {
       echo('<!--Running as stand alone WebApp on iPhone/iPad--><meta http-equiv="refresh" content="0; url=http://ichanger.tk/mainf7.php');
    } else if ( substr_count($ua, '/') === 2 ) {
-      echo('Running in a WebView on a iPhone/iPad app');
+      echo('<!--Running in a WebView on a iPhone/iPad app-->');
    } else {
-      echo('Running in another browser on iPhone/iPad');
+      echo('<!--Running in another browser on iPhone/iPad-->Chrome Dectected');
    }
 } else {
-   echo('<!--Running on device other than iPhone/iPad.--><meta http-equiv="refresh" content="0; url=http://ichanger.tk/pc/index.php" />');
+   echo('<!--Running on device other than iPhone/iPad.--><meta http-equiv="refresh" content="0; url=http://www.thelocken.com/ichangerinfo/" />');
 }
 ?>
