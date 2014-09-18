0b3f0600ca4034be2af47866f9378e9935b6a42d
Rosco1502 <thelocken@gmail.com>
2014-09-18 05:37:42 +0000

    centered chrome

--- 2/chrome.php
+++ 2/chrome.php
@@ -5,5 +5,4 @@ if(navigator.userAgent.match('CriOS')) {
 </script>-->
   <title>To install iChanger use Safari</title>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, minimal-ui">
-  <script type="text/javascript" src="http://ichanger.tk/js/staystand.js"></script>
-  <h2> To Install iChanger, Please Visit <a href="http://ichanger.tk/">iChanger.tk</a> in Safari.</h2>
+  <center><h2> To Install iChanger, Please Visit <a href="http://ichanger.tk/">iChanger.tk</a> in Safari.</h2></center>
