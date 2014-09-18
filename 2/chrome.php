1fa5fdc1e3ccf5d8cb4f207b3e6f787dd24624cd
Rosco1502 <thelocken@gmail.com>
2014-09-18 05:43:58 +0000

    centered chrome

--- 2/chrome.php
+++ 2/chrome.php
@@ -5,4 +5,17 @@ if(navigator.userAgent.match('CriOS')) {
 </script>-->
   <title>To install iChanger use Safari</title>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, minimal-ui">
-  <center><h2> To Install iChanger, Please Visit <a href="http://ichanger.tk/">iChanger.tk</a> in Safari.</h2></center>
+  <style>
+  #middle {
+    position:fixed;
+    top: 50%;
+    left: 50%;
+    width:30em;
+    height:18em;
+    margin-top: -9em; /*set to a negative number 1/2 of your height*/
+    margin-left: -15em; /*set to a negative number 1/2 of your width*/
+    border: 1px solid #ccc;
+    background-color: #f3f3f3;
+  }
+  </style>
+  <div style="middle"><center><h2> To Install iChanger, Please Visit <a href="http://ichanger.tk/">iChanger.tk</a> in Safari.</h2></center></div>
