3ccd3a8d04cbfefbcaa241c2e6bcafe5ebfe12fc
Rosco1502 <thelocken@gmail.com>
2014-09-18 03:59:56 +0000

    made chrome warning

--- 2/chrome.php
+++ 2/chrome.php
@@ -1,5 +1,9 @@
-<script>
+<!--<script>
 if(navigator.userAgent.match('CriOS')) {
     document.write('Hello');
 }
-</script>
+</script>-->
+  <title>To install iChanger use Safari</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, minimal-ui">
+  <script type="text/javascript" src="http://ichanger.tk/js/staystand.js"></script>
+  <h2> To Install iChanger, Please Visit <a href="http://ichanger.tk/">iChanger.tk</a> in Safari.</h2>
