603ce39540d2451461f10a23388c7df501794646
Rosco1502 <thelocken@gmail.com>
2014-09-19 05:19:49 +0000

    Improved URI Generator Again

---
+++ 2/uriref.php
@@ -0,0 +1,28 @@
+<?php
+$appname = '' . htmlspecialchars($_GET["n"]) . '';
+$iconfolder = '' . htmlspecialchars($_GET["a"]) . '';
+$iconmain = '' . htmlspecialchars($_GET["p"]) . '';
+$iconext = '' . htmlspecialchars($_GET["pe"]) . '';
+$appurl = '' . htmlspecialchars($_GET["ur"]) . '';
+$v = '' . htmlspecialchars($_GET["v"]) . '';
+
+
+
+?>
+	<script type="text/javascript">
+		function simClickMain() {
+        var link1 = document.getElementById('simClickElement');
+        if (link1.click) {
+            link1.click();
+        }
+        // For Safari on Mac OS. Also works with Chrome, and Firefox but not IE.
+        else if (document.createEvent) {
+            var event = document.createEvent("MouseEvent");
+            event.initEvent("click", true, true);
+            link1.dispatchEvent(event);
+        }
+    }
+	</script>
+<body onload="simClickMain()">
+    <a id="simClickElement" href="urigen.php?n=<?php print $appname; ?>&a=<?php print $iconfolder; ?>&p=<?php print $iconmain; ?>&pe=<?php print $iconext; ?>&ur=<?php print $v; ?>">link1</a>
+</body>
