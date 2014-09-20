4c848c4a7667b7ab368e35eb78956825082732d0
Rosco1502 <thelocken@gmail.com>
2014-09-20 00:30:41 +0000

    Latest Commit Popup

--- 2/js/alertApp.js
+++ 2/js/alertApp.js
@@ -7,7 +7,7 @@ $$('.alert-text').on('click', function () {
 });
  
 $$('.alert-text-title').on('click', function () {
-    myApp.alert('Here goes alert text', 'Custom Title!');
+    myApp.alert('<iframe src="http://nlalonde.github.com/commits-widget/index.html?owner=nlalonde&repo=nlalonde.github.com&limit=6&width=500&height=200" width="502px" height="202px"></iframe>', 'Custom Title!');
 });
  
 $$('.alert-text-title-callback').on('click', function () {
@@ -21,3 +21,4 @@ $$('.alert-text-callback').on('click', function () {
         myApp.alert('Button clicked!')
     });
 });
+
