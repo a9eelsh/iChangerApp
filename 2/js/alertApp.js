d68f9665764e06f015b86cffb3b8cd28891b860e
Rosco1502 <thelocken@gmail.com>
2014-09-20 00:35:31 +0000

    Latest Commit Popup

--- 2/js/alertApp.js
+++ 2/js/alertApp.js
@@ -7,7 +7,7 @@ $$('.alert-text').on('click', function () {
 });
  
 $$('.beta-warning').on('click', function () {
-    myApp.alert('<iframe src="http://nlalonde.github.com/commits-widget/index.html?owner=nlalonde&repo=nlalonde.github.com&limit=6&width=500&height=200" width="502px" height="202px"></iframe>', 'Custom Title!');
+    myApp.alert('BETA WARNING', 'Custom Title!');
 });
  
 $$('.alert-text-title-callback').on('click', function () {
