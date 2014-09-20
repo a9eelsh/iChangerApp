10ba6d22bf7008af1de10537d4b0a14564efdc73
Rosco1502 <thelocken@gmail.com>
2014-09-20 20:34:17 +0000

    Testing Offline Support

--- 2/uridata.php
+++ 2/uridata.php
@@ -34,7 +34,7 @@ $uri1 = 'data:text/html;charset=utf-8,';
 $uri2 = '<meta name="viewport" content="width=device-width, initial-scale=1.0"/>';
 $uri3 = '<meta name="apple-mobile-web-app-status-bar-style" content="black"/>';
 $uri4 = '<meta http-equiv="content-type" content="text/html;charset=utf-8"/>';
-$uri5 = '<link rel="apple-touch-icon" href="' . $iconfolder . '"/"' . $iconmain . '""' . $iconext . '">';
+$uri5 = '<link rel="apple-touch-icon" href=';
 
 echo $uri1;
 echo $uri2;
