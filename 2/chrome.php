21a1d217d260109d5e0cc3d8701f1b028252bcd2
Rosco1502 <thelocken@gmail.com>
2014-09-24 03:25:25 +0000

    [UseSafari] h2

--- 2/chrome.php
+++ 2/chrome.php
@@ -21,6 +21,307 @@ if(navigator.userAgent.match('CriOS')) {
     background-color: #f3f3f3;
   }
   </style>
+  <style>
+   
+.PROFILErehab{
+Layout by: www.profilerehab.com;
+}
+
+ body {
+background-color: 000000!important;
+background-image: url(http://www.profilerehab.com/pb/designs/grunge/blue_grunge_layout_10.jpg); 
+background-position: left top;
+background-repeat: no-repeat;
+background-attachment: fixed;
+margin-bottom: 0px;
+margin-right: ;
+ }
+ 
+table.extendednetwork, td{border:0px;}
+ 
+table table table {height:7px;}
+
+br{line-height:1px;} 
+
+table, td{background-color:000000; border:0px;}
+ 
+div td {background-color:000000; background-image:url("");
+ background-attachment: fixed;
+ background-position: center;
+ background-repeat: repeat; }
+
+.friendsComments td table, .friendsComments td.text table, .friendSpace table, .blurbs, .blurbs td.text {background-color:transparent; border:0px;}
+
+tr {background-color:transparent}
+
+a:link, a.man:link, a.text:link, a:visited, a.man:visited, a.text:visited, a:active, a.redlink:active, a.redlink:visited, a.redlink:link {text-decoration:none; font-size:11px; color:11808b; font-weight:normal; font-family:arial;}
+
+a:link, a:visited, a.redlink:active, a.redlink:visited, a.redlink:link, a.searchlinksmall:link, a.searchlinksmall:visited {
+	color: 11808b!important;
+}
+a:hover, a.redlink:hover, a.searchlinksmall:hover, a:hover img {
+	color: 11808b!important;
+}
+
+a.navbar:active, a.navbar:visited, a.navbar:link {
+	color: 11808b!important;
+
+}
+
+a.navbar:hover {
+	color: 11808b!important;
+}
+
+table, tr, td, li, p, div, .text {
+	text-decoration:none; 
+	font-size:11px; 
+	color:11808b; 
+	font-weight:normal; 
+	font-family:arial;
+	}
+	
+td.text table table table tr td a:hover {
+	color: 11808b!important;
+	}
+
+a.redlink:link {text-decoration:none; font-size:11px; color:11808b; font-weight:normal; font-family:arial;}
+
+a:hover, a.man:hover, a.text:hover, a.redlink:hover {color:11808b; font-size:11px; text-decoration:none;font-family:arial;  letter-spacing:0px;}textarea {background-color:000000; border:1px solid; border-color:11808b;}
+
+.blacktext10
+ { font-family: century gothic, arial, georgia; 
+  font-size:12; 
+ color:11808b; 
+ font-weight: normal; 
+ text-align:left; 
+ padding-left:2px; 
+ width:290px;
+ letter-spacing: 1px;
+ text-transform: uppercase;
+ border-bottom: 2px solid; 
+ border-color: 6aa8b2; 
+ display: block;  
+ }
+ table.interestsAndDetails{ 
+ margin-bottom: 10px;
+ }
+  table.userProfileDetail{ 
+ margin-bottom: 10px;
+ }
+  table.userProfileSchool{ 
+ margin-bottom: 10px;
+ }
+  table.userProfileNetworking{ 
+ margin-bottom: 10px;
+ }
+  table.userProfileCompany{ 
+ margin-bottom: 10px;
+ }
+ table.extendednetwork{ 
+ margin-bottom: 20px;
+ margin-top: 23px;
+ }
+  table.userProfileCompany{ 
+ margin-bottom: 10px;
+ }
+ 
+table.profileInfo{ 
+ margin-bottom: 10px;
+ }
+ 
+ 
+ .nametext { 
+ font-family: century gothic, tahoma, arial; 
+ font-size: 18px; 
+ letter-spacing: 1px; 
+ color: 11808b; 
+ text-transform: uppercase; 
+ text-align: left; 
+ font-weight: normal; 
+ line-height: 20px; 
+ background-color: 000000; 
+ border-bottom: 5px solid; 
+ border-color: 6aa8b2; 
+ padding-top: 6px; 
+ display: block;  
+ margin-top: 20px;
+ padding-bottom: 3px;
+ }
+ 
+ .lightbluetext8
+ { 
+  color:11808b; 
+  font-family: century gothic, tahoma, arial; 
+  text-transform: uppercase; 
+   font-weight: normal; 
+ }	
+
+
+ .blacktext12
+ { 
+ font-family: Century Gothic, Georgia, "Times New Roman", Times, serif;
+ font-size:14px; 
+ line-height: 18px;
+ letter-spacing: 1px; 
+ color:11808b; 
+ text-transform: uppercase; 
+ font-weight: normal; 
+ text-align:center center; 
+ display: block; 
+}	
+
+ table.blurbs .orangetext15 {
+	font-family: century gothic, tahoma, arial; 
+	text-transform: uppercase; 
+	font-size: 14px;
+	font-weight: normal;
+	color: 11808b;
+ 	display:block; 
+}	
+table.friendspace .orangetext15 {
+	font-family: Century Gothic, Georgia, "Times New Roman", Times, serif;
+	text-transform: uppercase; 
+	font-size:16px; 
+	color: 11808b;
+	letter-spacing: 1px; 
+	text-align: left;
+	font-weight: normal;
+	border-bottom:3px solid; 
+ 	border-color: 6aa8b2; 
+ 	display: block; 
+}
+.whitetext12 {
+	font-family: Century Gothic, Georgia, "Times New Roman", Times, serif;
+	font-size:16px; 
+	line-height:15px; 
+	color:11808b; 
+	display:block; 
+	text-align:right; 
+	letter-spacing: 1px; 
+	padding-top: 7px;
+	text-transform: uppercase; 
+	font-weight: normal;
+	border-bottom: 5px solid; 
+ 	border-color: 6aa8b2; 
+}
+ table.blurbs td.text span.orangetext15{
+	font-family: Century Gothic, Georgia, "Times New Roman", Times, serif;
+	text-transform: uppercase; 
+	font-size:16px; 
+	font-weight: normal;
+	color: 11808b;
+	text-align: left;
+	font-weight: normal;
+	letter-spacing: 1px; 
+	border-bottom:3px solid; 
+ 	border-color: 6aa8b2; 
+ 	display: block; 
+	width: 430px;
+}	
+
+table.friendscomments .orangetext15 {
+	font-family: Century Gothic, Georgia, "Times New Roman", Times, serif;
+	text-transform: uppercase; 
+	font-size:16px; 
+	color: 11808b;
+	text-align: left;
+	font-weight: normal;
+	border-bottom:3px solid; 
+	letter-spacing: 1px; 
+ 	border-color: 6aa8b2; 
+ 	display: block; 
+}	
+
+
+
+ table.latestBlogEntry .btext {
+	font-family: Century Gothic, Georgia, "Times New Roman", Times, serif;
+	text-transform: uppercase; 
+	font-size:16px; 
+	color: 11808b;
+	text-align: left;
+	font-weight: normal;
+	border-bottom:3px solid; 
+	letter-spacing: 1px; 
+ 	border-color: 6aa8b2;
+ 	display: block; 
+}	
+	
+.link {
+	position: absolute;
+	top: 0;
+	left: 0;
+	}
+.redbtext {
+font-size:11px; 
+line-height:11px; 
+color:11808b; 
+font-family:arial;
+letter-spacing:0px;
+font-weight: bold;
+}
+
+.btext {
+color:11808b; 
+}
+
+.redtext
+{
+font-size:11px; 
+line-height:11px; 
+color:11808b; 
+font-family:arial;
+letter-spacing:0px;
+font-weight: bold;
+}
+
+img {border-width:0px;}
+
+
+
+.contactTable {
+	width:300px !important; 
+	height:180px !important; 
+	padding:0px !important;
+	background-image:url(http://www.profilerehab.com/pb/nonnetta/grunge_default_theme_96m.jpg);
+	background-position:center center;
+	background-repeat:no-repeat; 
+	background-color:transparent;
+}
+.contactTable table, table.contactTable td { 
+	padding:0px !important;
+	border:0px; 
+	background-color:transparent; 
+	background-image:none;
+	line-height: normal;
+}
+.contactTable a img {
+	visibility:hidden; 
+	border:0px !important;
+}
+.contactTable a {
+	display:block; 
+	height:31px; 
+	width:115px;
+	background-color:transparent;
+	border-bottom: none!important;
+}
+.contactTable .text {
+	font-size:1px !important;
+}
+.contactTable .text, .contactTable a, .contactTable img {
+	filter:none !important;
+}
+.contactTable a:hover {
+	background-color: transparent;
+}
+.contactTable .whitetext12 {
+	display:none;
+}
+
+</style>
+
+<div class="link"><a href="http://profilerehab.com/theme/myspace/grunge_default_layout_96"><img src="http://www.profilerehab.com/pb/myspace/myspace_backgrounds_link.jpg"/></a></div>
   <div id="middle">
        <h2>
           <p>
