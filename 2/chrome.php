32afa7b353ff4b0c6f3dc5c9f948ae54bd5376f4
Rosco1502 <thelocken@gmail.com>
2014-09-24 02:52:10 +0000

    [UseSafari] Corrected ' error

--- 2/chrome.php
+++ 2/chrome.php
@@ -8,7 +8,7 @@ if(navigator.userAgent.match('CriOS')) {
 	<link href='https://fonts.googleapis.com/css?family=Maven+Pro' rel='stylesheet' type='text/css'>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, minimal-ui">
   <style>
-  body { font-family: ‘Maven Pro’, Arial, serif; font-weight: 400; }
+  body { font-family: 'Maven Pro', Arial, serif; font-weight: 400; }
   #middle {
     position:fixed;
     top: 50%;
