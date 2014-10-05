b8bebcd454862080517f12602fcac310b0eff18d
Rosco1502 <thelocken@gmail.com>
2014-10-05 07:08:00 +0000

    [Main] Status-Bar Bugs

--- 2/mainf7.php
+++ 2/mainf7.php
@@ -78,7 +78,7 @@ if ( $userraw == "" ) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, minimal-ui">
     <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <title>iChanger 2</title>
     <!-- Path to Framework7 Library CSS -->
     <link rel="stylesheet" href="ui/css/framework7.css">
