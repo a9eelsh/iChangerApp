460ab983864621307368e20fe6d12ba8e7fa6f00
Rosco1502 <thelocken@gmail.com>
2014-10-07 05:25:45 +0000

    [Photos] Fixed Icon Bug

--- 2/photosf7.php
+++ 2/photosf7.php
@@ -43,7 +43,7 @@ if ( $userraw == "" ) {
 				<li>
 					<a href="" class="already-istalled open-popover link item-link item-content">
 						<div class="item-media">
-							<img src="http://ichanger.tk/Photos/photos.png" width="80">
+							<img src="http://ichanger.tk/pics/photos.png" width="80">
 						</div>
 						<div class="item-inner">
 							<div class="item-title-row">
