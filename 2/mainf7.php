e309f74e9146e32088aa89992edb1cb00f4f888e
Rosco1502 <thelocken@gmail.com>
2014-10-10 04:52:59 +0000

    [Main] Updated About Popup

--- 2/mainf7.php
+++ 2/mainf7.php
@@ -883,6 +883,7 @@ If you feel that this site is not following its stated information policy, you m
 <p><ul><li>Bradley is the lead developer, he coded almost all of iChanger and also started TheLocken LLC, and iChanger</li><li>Loren is the main Android and Java coder of iChanger.</li><li>Jase is the main Icon-Gatherer, he sends icons he finds on various sources to our team to implement into iChanger.</li></ul></p>
 <p>We get icons form various sources, including (but not limited to) <a href="http://deviantart.com/" class="external close-popup">DiviantArt.com</a>, <a href="http://uiconstock.com/" class="external close-popup">UIconStock</a>, etc.</p>
 <p>Extra thanks to <a href="http://ffra.deviantart.com/" class="external close-popup">FFra</a> for supplying amazing icons packs such as Novus, Flatest, Maximal and to <a href="http://dtafalonso.deviantart.com/" class="external close-popup">Dtafalonso</a> for creating awesome packs such as iOS 7 Alternative, iOS 7 Icons (#1-7)</p>
+<p>Follow us on Twitter <a href="https://twitter.com/TheLockenLLC" class="external close-popup">@TheLockenLLC</a> and <a href="http://ichangerteam.deviantart.com/" class="external close-popup">@iChangerTeam</a> on DeviantArt.</p>
 </div>
 </div>
 </div>
