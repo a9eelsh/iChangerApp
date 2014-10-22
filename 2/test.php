7c07871448f562879f699b58dbf1150576bb7e94
Rosco1502 <thelocken@gmail.com>
2014-10-22 03:16:55 +0000

    [R][Main] Working on Facebook Test

--- 2/test.php
+++ 2/test.php
@@ -1,12 +1,10 @@
-<div class="views">
-<div class="view view-main">
-<div class="navbar">
-<div class="navbar-inner">
-<div class="left"></div>
-<div class="center sliding">Smart Select Custom</div>
-<div class="right"></div>
-</div>
-</div>
+        <div class="navbar">
+          <div class="navbar-inner">
+			      <div class="left sliding"><a href="mainf7.php<?php print $uend; ?>" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>
+            <div class="center sliding">iChanger 2 - Facebook</div>
+            <div class="right"><a href="#" class="open-panel link icon-only"><i class="icon icon-bars"></i></a></div>
+          </div>
+        </div>
 <div class="pages navbar-through">
 <div data-page="home" class="page">
 <div class="page-content">
