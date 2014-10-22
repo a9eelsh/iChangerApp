3efbebc11216576200fbd423f1aed72d2cd2b143
Rosco1502 <thelocken@gmail.com>
2014-10-22 03:15:30 +0000

    [R][Main] Working on Facebook Test

--- 2/test.php
+++ 2/test.php
@@ -1,13 +1,3 @@
-<html>
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, minimal-ui">
-<meta name="apple-mobile-web-app-capable" content="yes">
-<meta name="apple-mobile-web-app-status-bar-style" content="black">
-<title>My App</title>
-<link rel="stylesheet" href="http://www.idangero.us/framework7/dist/css/framework7.min.css">
-</head>
-<body>
 <div class="views">
 <div class="view view-main">
 <div class="navbar">
@@ -63,14 +53,3 @@
 </div>
 </div>
 </div>
-<script type="text/javascript" src="../dist/js/framework7.min.js"></script>
-<script>
-      var myApp = new Framework7({
-        animateNavBackIcon:true
-      }); 
-      var mainView = myApp.addView('.view-main', {
-        dynamicNavbar: true,
-      });
-    </script>
-</body>
-</html>
