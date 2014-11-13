c08c5471605b1191a8932e160aa54a5ddbd720c5
Rosco1502 <thelocken@gmail.com>
2014-11-13 07:16:00 +0000

    [R] Panel Reveal

--- 2/mainf7.php
+++ 2/mainf7.php
@@ -204,7 +204,7 @@ if ( $userraw == "" ) {
 
 
 <div class="panel-overlay"></div>
-<div class="panel panel-left panel-cover layout-dark">
+<div class="panel panel-left panel-cover layout-dark panel-reveal">
 <div class="content-block-title">MENU</div>
 <div class="list-block">
 <ul>
