a3c75ae4c32530f3e2de22ba3d284fcd4e9605cd
Rosco1502 <thelocken@gmail.com>
2014-11-13 06:35:23 +0000

    [B] Working on jQuery Tricks

--- 2/mainf7.php
+++ 2/mainf7.php
@@ -451,6 +451,17 @@ echo"Dev Mode Enabled";
 <form onsubmit="" action="http://www.ichanger.tk/custom/customend.php?req=custom" method="post" enctype="multipart/form-data">
   <ul>
     <!-- Text inputs -->
+    <script>
+$('#app').on('change',function(){
+    if( $(this).val()==="Facebook"){
+    $("#FBFunction").show()
+    }
+    else{
+    $("#FBFunction").hide()
+    }
+});
+    </script>
+    
     <li>
       <div class="item-content">
         <div class="item-media"><i class="icon icon-custom-title"></i></div>
@@ -533,7 +544,7 @@ echo"Dev Mode Enabled";
 </div>
 	<div class="content-block-title">Notice</div>
     <div class="content-block">
-        <p>Icons are deleted off our server after 10 hours of being uploaded to keep our servers happy.</p>
+        <p><div id="otherType" style="display:none;">Hello</div>Icons are deleted off our server after 10 hours of being uploaded to keep our servers happy.</p>
         <p><a href="#" data-popup=".popup-privacy" value="Create!" class="open-popup close-panel item-link">When you use iChanger you agree to the Privacy Policy</a></p>
     </div>
       </div>
