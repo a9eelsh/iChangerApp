14b83ecd010a5842b5e37c55d633f46dfecd1bc6
Rosco1502 <thelocken@gmail.com>
2014-11-14 03:15:09 +0000

    [B] General Code Cleanup

--- 2/mainf7.php
+++ 2/mainf7.php
@@ -164,7 +164,7 @@ if ( $userraw == "" ) {
 	</head>
   <body onload="simClickMain()">
     <!-- Pre loader -->
-    <div class="se-pre-con"><a class="preloader" id="preloader" href="loadslow.php">Loading Slow?</a></div>
+    <div class="se-pre-con"><a href="loadslow.php">Loading Slow?</a></div>
     <!-- If JS disabled redirect to ENABLEJS -->
 	  <noscript>
 	  	<meta http-equiv="refresh" content="0;URL='http://ichanger.tk/enable/javascript.php?no=js'" /> 
