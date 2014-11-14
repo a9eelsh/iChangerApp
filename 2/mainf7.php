c27cc508a3e13be6fbd680a20424789167f53d96
Rosco1502 <thelocken@gmail.com>
2014-11-14 07:28:28 +0000

    [B] Working on extream bug

--- 2/mainf7.php
+++ 2/mainf7.php
@@ -183,15 +183,7 @@ if ( $userraw == "" ) {
     
 <!-- 
 
-
-
-
                                                                     --------MENU--------
-
-
-
-
-
 
 
 
@@ -324,13 +316,6 @@ echo"Dev Mode Enabled";
 	  <!--
 
 
-
-
-
-
-
-
-
         -->
     <!-- Views, and they are tabs-->
     <!-- We need to set "toolbar-through" class on it to keep space for our tab bar-->
@@ -340,16 +325,7 @@ echo"Dev Mode Enabled";
         <!-- We can make with view with navigation, let's add Top Navbar-->
 		  <!--
 
- 
- 
- 
-
                   Home 
-
-
-
-
-
 
            -->
         <div class="navbar">
@@ -385,91 +361,9 @@ echo"Dev Mode Enabled";
           </div>
         </div>
       </div>
-<script>
-  $('#trigger').click(function () {
-    var clicks = $(this).data('clicks');
-    if (clicks) {
-        simClickMain3();
-    } else {
-
-    }
-    $(this).data("clicks", !clicks);
-});
-
-function simClickMain3() {
-    var link1 = document.getElementById('simClickElement3');
-    if (link1.click) {
-        link1.click();
-    }
-    // For Safari on Mac OS. Also works with Chrome, and Firefox but not IE.
-    else if (document.createEvent) {
-        var event = document.createEvent("MouseEvent");
-        event.initEvent("click", true, true);
-        link1.dispatchEvent(event);
-    }
-}
-
-              </div>
-              <div class="list-block">
-                <ul>
-                  <li><a href="about.html<?php print $uend; ?>" class="item-link">
-                      <div class="item-content">
-                        <div class="item-inner">
-                          <div class="item-title">About Us</div>
-                        </div>
-                      </div></a></li>
-                  <li><a href="services.html<?php print $uend; ?>" class="item-link">
-                      <div class="item-content">
-                        <div class="item-inner">
-                          <div class="item-title">Services</div>
-                        </div>
-                      </div></a></li>
-                </ul>
-                <center><iframe src="http://nlalonde.github.com/commits-widget/index.html?owner=rosco1502&repo=iChanger-2&limit=6&width=200&height=200" width="502px" height="202px"></iframe></center>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
 <!-- 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
                                                                           CUSTOM
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 -->
       <div id="view-2" class="view tab">
@@ -693,29 +587,10 @@ $('#YTResult').on('change',function(){
 </script>
       <div id="view-3" class="view view-main tab active">
         <!-- We can make with view with navigation, let's add Top Navbar-->
-		  
-		  <!--
-
-
-
-
-
-
-
-
-
-                                                                        PreMade
-
-
-
-
-
-
-
-
-
-
-          -->
+        
+        <!-- PREMADE -->
+        
+        
         <div class="navbar">
           <div class="navbar-inner">
             <div class="center sliding">iChanger - Premade Icons 
@@ -968,18 +843,7 @@ $('#YTResult').on('change',function(){
           <div data-page="index-4" class="page">
 			  <!--
 
-
-
-
-
-
                                                                           iPeeps
-
-
-
-
-
-
                 -->
             <div class="navbar">
               <div class="navbar-inner">
@@ -1107,19 +971,9 @@ $('#YTResult').on('change',function(){
         </div>
       </div>
       <!--
-      
-      
-      
-      
-      
-      
+
                                                                         Bottom Tabbar
-      
-      
-      
-      
-      
-      
+
       -->
 <div class="toolbar tabbar tabbar-labels">
     <div class="toolbar-inner">
@@ -1140,14 +994,9 @@ $('#YTResult').on('change',function(){
     </div>
 </div>
 	<!--
-	
-	
-	
+
 	
 	                                                                          Popups
-	
-	
-	
 	
 	
 	-->
@@ -1184,31 +1033,6 @@ $('#YTResult').on('change',function(){
 </div>
 </div>
 </div>
-  <!-- Terms of Service Popup -->
-  <div class="popup popup-terms">
-<div class="view navbar-fixed">
-<div class="pages">
-<div class="page">
-<div class="navbar">
-<div class="navbar-inner">
-<div class="center">Terms of Service</div>
-<div class="right"><a href="#" class="link close-popup">Done</a></div>
-</div>
-</div>
-<div class="page-content">
-<div class="content-block">
-<p>Here comes popup. You can put here anything, even independent view with its own navigation. Also not, that by default popup looks a bit different on iPhone/iPod and iPad, on iPhone it is fullscreen.</p>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse faucibus mauris leo, eu bibendum neque congue non. Ut leo mauris, eleifend eu commodo a, egestas ac urna. Maecenas in lacus faucibus, viverra ipsum pulvinar, molestie arcu. Etiam lacinia venenatis dignissim. Suspendisse non nisl semper tellus malesuada suscipit eu et eros. Nulla eu enim quis quam elementum vulputate. Mauris ornare consequat nunc viverra pellentesque. Aenean semper eu massa sit amet aliquam. Integer et neque sed libero mollis elementum at vitae ligula. Vestibulum pharetra sed libero sed porttitor. Suspendisse a faucibus lectus.</p>
-<p>Duis ut mauris sollicitudin, venenatis nisi sed, luctus ligula. Phasellus blandit nisl ut lorem semper pharetra. Nullam tortor nibh, suscipit in consequat vel, feugiat sed quam. Nam risus libero, auctor vel tristique ac, malesuada ut ante. Sed molestie, est in eleifend sagittis, leo tortor ullamcorper erat, at vulputate eros sapien nec libero. Mauris dapibus laoreet nibh quis bibendum. Fusce dolor sem, suscipit in iaculis id, pharetra at urna. Pellentesque tempor congue massa quis faucibus. Vestibulum nunc eros, convallis blandit dui sit amet, gravida adipiscing libero.</p>
-<p>Morbi posuere ipsum nisl, accumsan tincidunt nibh lobortis sit amet. Proin felis lorem, dictum vel nulla quis, lobortis dignissim nunc. Pellentesque dapibus urna ut imperdiet mattis. Proin purus diam, accumsan ut mollis ac, vulputate nec metus. Etiam at risus neque. Fusce tincidunt, risus in faucibus lobortis, diam mi blandit nunc, quis molestie dolor tellus ac enim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse interdum turpis a velit vestibulum pharetra. Vivamus blandit dapibus cursus. Aenean lorem augue, vehicula in eleifend ut, imperdiet quis felis.</p>
-<p>Duis non erat vel lacus consectetur ultricies. Sed non velit dolor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin vel varius mi, a tristique ante. Vivamus eget nibh ac elit tempor bibendum sit amet vitae velit. Proin sit amet dapibus nunc, non porta tellus. Fusce interdum vulputate imperdiet. Sed faucibus metus at pharetra fringilla. Fusce mattis orci et massa congue, eget dapibus ante rhoncus. Morbi semper sed tellus vel dignissim. Cras vestibulum, sapien in suscipit tincidunt, lectus mi sodales purus, at egestas ligula dui vel erat. Etiam cursus neque eu lectus eleifend accumsan vitae non leo. Aliquam scelerisque nisl sed lacus suscipit, ac consectetur sapien volutpat. Etiam nulla diam, accumsan ut enim vel, hendrerit venenatis sem. Vestibulum convallis justo vitae pharetra consequat. Mauris sollicitudin ac quam non congue.</p>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
 <!-- About Popup -->
 <div class="popup popup-about">
 <div class="view navbar-fixed">
@@ -1236,20 +1060,9 @@ $('#YTResult').on('change',function(){
 </div>
 </div>
 </div>
-<!-- Credits Popup -->
-<div class="popup popup-credits">
-<div class="view navbar-fixed">
-<div class="pages">
-<div class="page">
-<div class="navbar">
-<div class="navbar-inner">
-<div class="center">Popup Title</div>
-<div class="right"><a href="#" class="link close-popup">Done</a></div>
-</div>
-</div>
-<div class="page-content">
-<div class="content-block">
-<p>Here comes popup. You can put here anything, even independent view with its own navigation. Also not, that by default popup looks a bit different on iPhone/iPod and iPad, on iPhone it is fullscreen.</p>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse faucibus mauris leo, eu bibendum neque congue non. Ut leo mauris, eleifend eu commodo a, egestas ac urna. Maecenas in lacus faucibus, viverra ipsum pulvinar, molestie arcu. Etiam lacinia venenatis dignissim. Suspendisse non nisl semper tellus malesuada suscipit eu et eros. Nulla eu enim quis quam elementum vulputate. Mauris ornare consequat nunc viverra pellentesque. Aenean semper eu massa sit amet aliquam. Integer et neque sed libero mollis elementum at vitae ligula. Vestibulum pharetra sed libero sed porttitor. Suspendisse a faucibus lectus.</p>
-<p>Duis ut mauris sollicitudin, venenatis nisi sed, luctus ligula. Phasellus blandit nisl ut lorem semper pharetra. Nullam tortor nibh, suscipit in consequat vel, feugiat sed quam. Nam risus libero, auctor vel tristique ac, malesuada ut ante. Sed molestie, est in eleifend sagittis, leo tortor ullamcorper erat, at vulputate eros sapien nec libero. Mauris dapibus laoreet nibh quis bibendum. Fusce dolor sem, suscipit in iaculis id, pharetra at urna. Pellentesque tempor congue massa quis faucibus. Vestibulum nunc eros, convallis blandit dui sit amet, gravida adipiscing libero.</p>
-<p>Morbi posuere ipsum nisl, accumsan tincidunt nibh lobortis sit amet. Proin felis lorem, dictum vel nulla quis, lobortis dignissim nunc. Pellentesque dapibus urna ut imperdiet mattis. Proin puru
+    <!-- Path to Framework7 Library JS-->
+    <script type="text/javascript" src="js/framework7.min.js"></script>
+    <script type="text/javascript" src="js/f7-2.js"></script>
+    <script type="text/javascript" src="http://www.ichanger.tk/js/alerts.js?v=3"></script>
+  </body>
+</html>
