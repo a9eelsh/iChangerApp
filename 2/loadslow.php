342a611942f86c129ce0b9db4dca060a8ebc1261
Rosco1502 <thelocken@gmail.com>
2014-11-14 03:39:56 +0000

    [B] Centered Loading Slow?

--- 2/loadslow.php
+++ 2/loadslow.php
@@ -38,18 +38,152 @@
           <div data-page="index" class="page">
             <!-- Scrollable page content -->
             <div class="page-content">
-              <p>Page content goes here</p>
-              <!-- Link to another page -->
-              <a href="about.html">About app</a>
+              <div class="content-block-title">LOADING SLOW SEE WHY</div>
+              <div class="content-block inset">
+                <p>TheLocken hosts iChanger using high speed Content Delivery Networks (CDN) such as CloudFlare CDN and DreamSpeed CDN so most likely this is a problem on your end. Check you connectivity to WiFi or make sure you have 3G or 4G Network Speeds. If the problem still occors after you get a strong network connection then you should contact us or file a bug report <a href="http://bit.ly/iC2newissue">here</a>.</p>
+              </div>
+              <div class="content-block tablet-inset">
+<h1 id="main-title" itemprop="headline">iOS: Troubleshooting Wi-Fi networks and connections</h1>
+<div class="intro"><p>Learn how to troubleshoot Wi-Fi<sup><a href="#footnote" onclick="s_objectID=&quot;http://support.apple.com/en-us/TS1398#footnote_1&quot;;return this.s_oc?this.s_oc(e):true">1</a></sup> issues you may encounter while using your iPhone, iPad, or iPod touch.</p></div>
+
+<div itemprop="author" itemscope="" itemtype="http://schema.org/Organization">
+  <meta itemprop="name" content="Apple">
+</div>
+<div id="sections" itemprop="articleBody">
+<section class=""><p>Occasionally while using your iOS device, you may notice unexpected Wi-Fi behavior:</p>
+<ul>
+    <li>Difficulty locating or connecting to a network</li>
+    <li>Unexpected Wi-Fi signal strength or disconnects</li>
+    <li>Inability to  access the Internet</li>
+    <li>Not automatically connecting to Wi-Fi when expected</li>
+</ul>
+<p>Follow these steps to troubleshoot the above issues:</p>
+<ol>
+    <li>Be sure you're in range of your Wi-Fi router (access point).</li>
+    <li>Tap <strong>Settings &gt; Wi-Fi</strong> and turn Wi-Fi off and on. If your Wi-Fi setting is dimmed, <a href="http://support.apple.com/kb/TS1559" onclick="s_objectID=&quot;http://support.apple.com/kb/TS1559_1&quot;;return this.s_oc?this.s_oc(e):true">follow these steps</a>.</li>
+    <li>Confirm that your Wi-Fi router and cable or DSL modem are connected to power, turned on, and connected to the Internet. If not, refer to your network administrator or Internet service provider (ISP) for assistance.</li>
+    <li><a href="http://support.apple.com/kb/HT1430" onclick="s_objectID=&quot;http://support.apple.com/kb/HT1430_1&quot;;return this.s_oc?this.s_oc(e):true">Restart your iOS device</a>.</li>
+    <li>Tap <strong>Settings &gt; Wi-Fi</strong> and locate the Wi-Fi network to which you're  connected.</li>
+    <li>Tap <img width="24" height="22" align="absBottom" alt="" src="http://km.support.apple.com/library/APPLE/APPLECARE_ALLGEOS/TS1398/TS1398_i.png" data-hires="true"> and <strong>Forget this Network</strong>.</li>
+    <li>Try to connect to your desired Wi-Fi network. <br>
+    <strong>Note</strong>: You may need to enter your Wi-Fi password again if your network requires one.</li>
+    <li>Turn your Wi-Fi router  off and on<sup><a href="#footnote" onclick="s_objectID=&quot;http://support.apple.com/en-us/TS1398#footnote_2&quot;;return this.s_oc?this.s_oc(e):true">2</a></sup>.  If your ISP also provides cable or phone service, check with them before attempting this step to avoid interruption of service.</li>
+    <li><a href="http://support.apple.com/kb/HT4623" onclick="s_objectID=&quot;http://support.apple.com/kb/HT4623_1&quot;;return this.s_oc?this.s_oc(e):true">Update your device</a> to the latest version of software.</li>
+    <li>Update your Wi-Fi router to the latest firmware<sup><a href="#footnote" onclick="s_objectID=&quot;http://support.apple.com/en-us/TS1398#footnote_3&quot;;return this.s_oc?this.s_oc(e):true">2</a></sup>. For AirPort Base Stations, <a href="http://support.apple.com/kb/HT1218" onclick="s_objectID=&quot;http://support.apple.com/kb/HT1218_1&quot;;return this.s_oc?this.s_oc(e):true">install updates using the AirPort Utility</a>.</li>
+</ol>
+<h2>If the issue is still unresolved</h2>
+<p>Follow the steps below for the issue you're experiencing.</p>
+<p><a href="javascript:ACUtil.collapseAll()" onclick="s_objectID=&quot;javascript:ACUtil.collapseAll()_1&quot;;return this.s_oc?this.s_oc(e):true">Collapse All</a> | <a href="javascript:ACUtil.expandAll();" onclick="s_objectID=&quot;javascript:ACUtil.expandAll();_1&quot;;return this.s_oc?this.s_oc(e):true">Expand All</a></p>
+<h3 class="collapse">Unable to locate a Wi-Fi network</h3>
+<div>
+<ol>
+    <li>Verify that the network is available by tapping <strong>Settings &gt; Wi-Fi</strong> and choosing from the available networks. <strong>Note:</strong> It may take a few seconds for the Wi-Fi network name to appear.</li>
+    <li>Move closer to your wireless router (access point) and attempt to locate the Wi-Fi network.</li>
+    <li>If you do not see the network you would like to join, you may be attempting to connect to a hidden network.  Learn <a href="http://support.apple.com/kb/HT5569" onclick="s_objectID=&quot;http://support.apple.com/kb/HT5569_1&quot;;return this.s_oc?this.s_oc(e):true">how to join a hidden network</a>.</li>
+    <li>Supported Wi-Fi configurations vary by iOS   device model.  Find out <a href="http://support.apple.com/specs/" onclick="s_objectID=&quot;http://support.apple.com/specs/_1&quot;;return this.s_oc?this.s_oc(e):true">which standards</a> your device supports<sup><a href="#footnote" onclick="s_objectID=&quot;http://support.apple.com/en-us/TS1398#footnote_4&quot;;return this.s_oc?this.s_oc(e):true">3</a></sup>.</li>
+    <li>Reset network settings by tapping <strong>Settings &gt; General &gt; Reset &gt; Reset Network Settings</strong>. <strong>Note:</strong> This will reset all network settings including:
+    <ul>
+        <li>previously connected Wi-Fi networks and passwords</li>
+        <li>recently used Bluetooth accessories</li>
+        <li>VPN and APN settings</li>
+    </ul>
+    </li>
+</ol>
+</div>
+<h3 class="collapse">Unable to connect to a Wi-Fi network</h3>
+<div>
+<ol>
+    <li>Verify that  you're attempting to connect to your desired Wi-Fi network.</li>
+    <li>Make sure you're entering your Wi-Fi password correctly. Passwords may be case sensitive and may contain numbers or special characters.</li>
+    <li>Reset network settings by tapping <strong>Settings &gt; General &gt; Reset &gt; Reset Network Settings</strong>. <strong>Note:</strong> This will reset all network settings including:
+    <ul>
+        <li>previously connected Wi-Fi networks and passwords</li>
+        <li>recently used Bluetooth accessories</li>
+        <li>VPN and APN settings</li>
+    </ul>
+    </li>
+</ol>
+</div>
+<h3 class="collapse">Wi-Fi disconnects or signal strength is less than expected</h3>
+<div>
+<ol>
+    <li>Move closer to the Wi-Fi router (access point).</li>
+    <li>Check for sources of <a href="http://support.apple.com/kb/HT1365" onclick="s_objectID=&quot;http://support.apple.com/kb/HT1365_1&quot;;return this.s_oc?this.s_oc(e):true">potential interference</a>.</li>
+    <li>Remove any case, stand, or other accessories from your iOS device and see if signal strength improves.</li>
+    <li>Reset network settings by tapping <strong>Settings &gt; General &gt; Reset &gt; Reset Network Settings</strong>. <strong>Note:</strong> This will reset all network settings including
+    <ul>
+        <li>previously connected Wi-Fi networks and passwords</li>
+        <li>recently used Bluetooth accessories</li>
+        <li>VPN and APN settings</li>
+    </ul>
+    </li>
+</ol>
+</div>
+<h3 class="collapse">Unable to access the Internet while connected to Wi-Fi</h3>
+<div>
+<ol>
+    <li>If you're using a public or commercial network, you may need to <a href="http://support.apple.com/kb/HT3867" onclick="s_objectID=&quot;http://support.apple.com/kb/HT3867_1&quot;;return this.s_oc?this.s_oc(e):true">log in or subscribe</a>.</li>
+    <li>Check to see if you have a self-assigned IP address:
+    <ol type="a">
+        <li>Tap <strong>Settings &gt; Wi-Fi</strong> and locate the Wi-Fi network to which you're connected and tap <img width="24" height="22" align="absBottom" src="http://km.support.apple.com/library/APPLE/APPLECARE_ALLGEOS/TS1398/TS1398_i.png" alt="" data-hires="true">.</li>
+        <li>If your IP   address is 169.254.xxx.xxx, <a href="http://support.apple.com/kb/TS3798" onclick="s_objectID=&quot;http://support.apple.com/kb/TS3798_1&quot;;return this.s_oc?this.s_oc(e):true">you may not be able to access the Internet</a>.</li>
+    </ol>
+    </li>
+    <li>If you do not have a self-assigned IP address, see if other Wi-Fi  devices on your network have Internet access.
+    <ul>
+        <li>If they do not, consult with your network administrator or ISP for further assistance.</li>
+        <li>If they do, reset network settings on your device by tapping <strong>Settings &gt; General &gt; Reset &gt; Reset Network Settings</strong>. <strong>Note:</strong> This will reset all network settings including:
+        <ul>
+            <li>previously connected Wi-Fi networks and passwords</li>
+            <li>recently used Bluetooth accessories</li>
+            <li>VPN and APN settings</li>
+        </ul>
+        </li>
+    </ul>
+    </li>
+</ol>
+</div>
+<h3 class="collapse">Unable to automatically connect to Wi-Fi</h3>
+<div>
+<ul>
+    <li><a href="http://support.apple.com/kb/HT4450" onclick="s_objectID=&quot;http://support.apple.com/kb/HT4450_1&quot;;return this.s_oc?this.s_oc(e):true">Avoid giving your Wi-Fi network a common name</a>.
+    <ul>
+        <li>Some wireless routers are preconfigured with a common SSID (network name) such as "wireless" or "default."</li>
+        <li>If your desired Wi-Fi network has a common SSID, try changing the SSID to something unique<sup><a href="#footnote" onclick="s_objectID=&quot;http://support.apple.com/en-us/TS1398#footnote_5&quot;;return this.s_oc?this.s_oc(e):true">2</a></sup>.</li>
+    </ul>
+    </li>
+    <li>If your desired Wi-Fi network has a unique SSID, reset network settings by tapping <strong>Settings &gt; General &gt; Reset &gt; Reset Network Settings</strong>. <strong>Note:</strong> This will reset all network settings including:
+    <ul>
+        <li>previously connected Wi-Fi networks and passwords</li>
+        <li>recently used Bluetooth accessories</li>
+        <li>VPN and APN settings</li>
+    </ul>
+    </li>
+</ul>
+</div></section><section class=""><ul>
+    <li>Find information on <a href="https://support.apple.com/kb/HT5569" onclick="s_objectID=&quot;https://support.apple.com/kb/HT5569_1&quot;;return this.s_oc?this.s_oc(e):true">connecting to a Wi-Fi network</a><a href="https://support.apple.com/kb/HT5569" onclick="s_objectID=&quot;https://support.apple.com/kb/HT5569_2&quot;;return this.s_oc?this.s_oc(e):true">.</a></li>
+    <li>Some third-party routers may not be compatible with all iOS devices, even after updating to the latest firmware. If your iOS  devices can successfully connect to other Wi-Fi networks, contact your  router's manufacturer to ensure that your router is compatible with your iOS device.</li>
+    <li>See recommended settings for <a href="http://support.apple.com/kb/HT4199" onclick="s_objectID=&quot;http://support.apple.com/kb/HT4199_1&quot;;return this.s_oc?this.s_oc(e):true">configuring your Wi-Fi router for use with iOS devices</a>.</li>
+</ul>
+<p><a id="footnote" name="footnote"></a></p>
+<p><strong>Notes</strong></p>
+<ol class="sosumi">
+    <li>iOS devices sold in China may use the term Wireless LAN (WLAN) instead of Wi-Fi. Not all China iPhone models support <a href="http://support.apple.com/kb/HT3893" onclick="s_objectID=&quot;http://support.apple.com/kb/HT3893_1&quot;;return this.s_oc?this.s_oc(e):true">WLAN</a>.</li>
+    <li>If needed, please consult with your network administrator (which may be your Internet Service Provider) for assistance with this step.</li>
+    <li>iPhone 5, iPad devices, and iPod touch (5th generation) are the only iOS devices that can connect to 5 GHz networks.</li>
+</ol></section>
+</div>
+<div class="sosumi"><strong>Important:</strong> Information about products not manufactured by Apple is provided for information purposes only and does not constitute Apple’s recommendation or endorsement. Please <a href="http://support.apple.com/kb/HT2693?viewlocale=en_US" onclick="s_objectID=&quot;http://support.apple.com/kb/HT2693?viewlocale=en_US_1&quot;;return this.s_oc?this.s_oc(e):true">contact the vendor</a> for additional information.</div>
+</div>
+              
+              
+              
+              
+              
+              
+              
+              
             </div>
-          </div>
-        </div>
-        <!-- Bottom Toolbar-->
-        <div class="toolbar">
-          <div class="toolbar-inner">
-            <!-- Toolbar links -->
-            <a href="#" class="link">Link 1</a>
-            <a href="#" class="link">Link 2</a>
           </div>
         </div>
       </div>
