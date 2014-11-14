8f0749422b44b4c56f77f366ad5f0353ce77eec6
Rosco1502 <thelocken@gmail.com>
2014-11-14 03:19:07 +0000

    [B] Centered Loading Slow?

--- 2/mainf7.php
+++ 2/mainf7.php
@@ -164,7 +164,7 @@ if ( $userraw == "" ) {
 	</head>
   <body onload="simClickMain()">
     <!-- Pre loader -->
-    <div class="se-pre-con"><a style="width:800px; margin:0 auto;" href="loadslow.php">Loading Slow?</a></div>
+    <div class="se-pre-con"><center><a href="loadslow.php">Loading Slow?</a></center></div>
     <!-- If JS disabled redirect to ENABLEJS -->
 	  <noscript>
 	  	<meta http-equiv="refresh" content="0;URL='http://ichanger.tk/enable/javascript.php?no=js'" /> 
