fdb095a60fb565b5c9c2429ada804d85c201d9ba
Rosco1502 <thelocken@gmail.com>
2014-11-16 11:01:32 +0000

    [B] Working on new loading animation

--- 2/mainf7.php
+++ 2/mainf7.php
@@ -99,29 +99,106 @@ if ( $userraw == "" ) {
 	  <link rel="stylesheet" href="ui/css/closex.css">
 	  <link rel="stylesheet" href="ui/css/load.css">
 	  <style>
-	    .no-js #loader { display: none;  }
-      .js #loader { display: block; position: absolute; left: 100px; top: 0; }
-      .se-pre-con {
-         position: fixed;
-        left: 0px;
-        top: 0px;
-        width: 100%;
-        height: 100%;
-        z-index: 9999;
-        background: url(http://ichanger.tk/ui/simages/loader1.gif) center no-repeat #fff;
-      }
-      
-      #preloader {
-        height: 200px;
-        width: 400px;
-        background: black;
-  
-        position: fixed;
-        top: 50%;
-        left: 50%;
-        margin-top: -100px;
-        margin-left: -200px;
-      }
+.loading  {
+    background: rgba(0, 50, 250, 0);
+    position: relative;
+    margin: 5em auto 0 auto;
+    width: 3em; 
+    height: 3em;
+    animation-name:rotate;
+}
+ 
+.loading,
+.loading:before,
+.loading:after  {
+    border-radius: 100%;
+    animation-duration: 3s;
+    animation-iteration-count: infinite;
+    animation-timing-function: ease-in;
+}
+ 
+.loading:before,
+.loading:after  {
+    content: "";
+    position: absolute;
+    top: 0; 
+    left: 0;
+    width: inherit; 
+    height: inherit;
+}
+ 
+.loading:before  {
+    background: rgba(200, 250, 100, 0);
+    animation-name: ring;
+}
+ 
+.loading:after  {
+    background: rgba(250, 0, 200, 0);
+    animation-name: ring2;
+}
+ 
+@keyframes rotate  {
+    0%  {
+        transform: rotateZ(0deg) scaleX(0.1) scaleY(0.1) translateZ(0);
+        box-shadow: inset 0.8em 0 0 rgba(255, 0, 0, 0.5), 
+                    inset 0 0.8em 0 rgba(252, 150, 0, 0.5), 
+                    inset -0.8em 0 0 rgba(0, 255, 0, 0.5), 
+                    inset 0 -0.8em 0 rgba(0, 150, 255, 0.5);
+    }
+     
+    /* hidden */
+     
+    85%, 100%  {
+     
+    /* 360deg * 10 */
+     
+        transform: rotateZ(3600deg) scaleX(2.01) scaleY(2) translateZ(0);
+        box-shadow: inset 0 0 0 rgba(255, 0, 0, 0), 
+                    inset 0 0 0 rgba(252, 150, 0, 0), 
+                    inset 0 0 0 rgba(0, 255, 0, 0), 
+                    inset 0 0 0 rgba(0, 150, 255, 0);
+    }
+}
+ 
+@keyframes ring  {
+    0%  {
+        transform: scaleX(0.1) scaleY(0.5);
+        box-shadow: inset 0.8em 0 0 rgba(255, 0, 0, 0.5), 
+                    inset 0 0.8em 0 rgba(252, 150, 0, 0.5), 
+                    inset -0.8em 0 0 rgba(0, 255, 0, 0.5), 
+                    inset 0 -0.8em 0 rgba(0, 150, 255, 0.5);
+    }
+     
+    /* hidden */
+     
+    75%, 100%  {
+        transform: scaleX(2) scaleY(2.1);
+        box-shadow: inset 0 0 0 rgba(255, 0, 0, 0), 
+                    inset 0 0 0 rgba(252, 150, 0, 0), 
+                    inset 0 0 0 rgba(0, 255, 0, 0), 
+                    inset 0 0 0 rgba(0, 150, 255, 0);
+    }
+}
+ 
+@keyframes ring2  {
+    0%  {
+        transform: scaleX(0.5) scaleY(0.1);
+        box-shadow: inset 0.8em 0 0 rgba(255, 0, 0, 0.5), 
+                    inset 0 0.8em 0 rgba(252, 150, 0, 0.5), 
+                    inset -0.8em 0 0 rgba(0, 255, 0, 0.5), 
+                    inset 0 -0.8em 0 rgba(0, 150, 255, 0.5);
+    }
+     
+    /* hidden */
+     
+    65%, 100%  {
+        transform: scaleX(2) scaleY(2.1);
+        box-shadow: inset 0 0 0 rgba(255, 0, 0, 0), 
+                    inset 0 0 0 rgba(252, 150, 0, 0), 
+                    inset 0 0 0 rgba(0, 255, 0, 0), 
+                    inset 0 0 0 rgba(0, 150, 255, 0);
+    }
+}
 	  </style>
 	  <!-- JS -->
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
@@ -164,13 +241,13 @@ if ( $userraw == "" ) {
       // Wait for window load
         $(window).load(function() {
         // Animate loader off screen
-        $(".se-pre-con").fadeOut("slow");;
+        $(".loading").fadeOut("slow");;
       });
     </script>
 	</head>
   <body onload="simClickMain()">
     <!-- Pre loader -->
-    <div class="se-pre-con"><p></p><p><center><a class="external" href="loadslow.php">Loading Slow? See Why</a></center></p></div>
+    <div class="loading"><p></p><p><center><a class="external" href="loadslow.php">Loading Slow? See Why</a></center></p></div>
     <!-- If JS disabled redirect to ENABLEJS -->
 	  <noscript>
 	  	<meta http-equiv="refresh" content="0;URL='http://ichanger.tk/enable/javascript.php?no=js'" /> 
@@ -403,10 +480,9 @@ echo"Dev Mode Enabled";
       <div class="item-content">
         <div class="item-media"><i class="icon icon-custom-app"></i></div>
         <div class="item-inner">
-          <div class="item-title label">App to Open</div>
+          <div class="item-title label">Open</div>
           <div class="item-input">
             <select id="app" name="app">
-              <option>-Choose App-</option>
               <option>App Store</option>
               <option>Calendar</option>
               <option>Facetime</option>
@@ -433,6 +509,7 @@ echo"Dev Mode Enabled";
               <option>Photos</option>
               <option>Google</option>
               <option>Google+</option>
+              <option>iChanger</option>
               <option>URI Scheme</option>
             </select>
           </div>
@@ -460,7 +537,6 @@ echo"Dev Mode Enabled";
           <div class="item-title label">Function</div>
           <div class="item-input">
             <select id="FBResult" name="func">
-              <option>----</option>
               <option>Your Profile</option>
               <option>Friends List</option>
               <option>Notification List</option>
@@ -532,6 +608,21 @@ echo"Dev Mode Enabled";
     </li>
     </div>
     <!--           END YOUTUBE VIDEO ID                           -->
+    <!--           CUSTOM URI SCHEME ID                           -->
+    <div id="UriScheme" style="display:none;">
+    <li>
+      <div class="item-content">
+        <div class="item-media"><i class="icon icon-custom-uri"></i></div>
+        <div class="item-inner">
+          <div class="item-title label">URI</div>
+            <div class="item-input">
+              <input type="text" name="customuri" id="customuri" placeholder="gamecenter://">
+            </div>  
+        </div>
+      </div>
+    </li>
+    </div>
+    <!--          END CUSTOM URI SCHEME ID                        -->
   </ul>
 	<input type="submit" name="submit" value="Create!" class="button button-big button-round color-red">
 	</form>
@@ -569,6 +660,14 @@ $('#app').on('change',function(){
     }
     else{
     $("#YouTubeFunction").hide()
+    }
+});
+$('#app').on('change',function(){
+    if( $(this).val()==="URI Scheme"){
+    $("#UriScheme").show()
+    }
+    else{
+    $("#UriScheme").hide()
     }
 });
 $('#FBResult').on('change',function(){
