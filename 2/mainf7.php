2f89ab41b38f89a2049e2c708fde3e1aa112ee90
Rosco1502 <thelocken@gmail.com>
2014-11-16 19:10:00 +0000

    [B] Working on new loading animation

--- 2/mainf7.php
+++ 2/mainf7.php
@@ -100,105 +100,7 @@ if ( $userraw == "" ) {
 	  <link rel="stylesheet" href="ui/css/load.css">
 	  <style>
 .loading  {
-    background: rgba(0, 50, 250, 0);
-    position: relative;
-    margin: 5em auto 0 auto;
-    width: 3em; 
-    height: 3em;
-    animation-name:rotate;
-}
- 
-.loading,
-.loading:before,
-.loading:after  {
-    border-radius: 100%;
-    animation-duration: 3s;
-    animation-iteration-count: infinite;
-    animation-timing-function: ease-in;
-}
- 
-.loading:before,
-.loading:after  {
-    content: "";
-    position: absolute;
-    top: 0; 
-    left: 0;
-    width: inherit; 
-    height: inherit;
-}
- 
-.loading:before  {
-    background: rgba(200, 250, 100, 0);
-    animation-name: ring;
-}
- 
-.loading:after  {
-    background: rgba(250, 0, 200, 0);
-    animation-name: ring2;
-}
- 
-@keyframes rotate  {
-    0%  {
-        transform: rotateZ(0deg) scaleX(0.1) scaleY(0.1) translateZ(0);
-        box-shadow: inset 0.8em 0 0 rgba(255, 0, 0, 0.5), 
-                    inset 0 0.8em 0 rgba(252, 150, 0, 0.5), 
-                    inset -0.8em 0 0 rgba(0, 255, 0, 0.5), 
-                    inset 0 -0.8em 0 rgba(0, 150, 255, 0.5);
-    }
-     
-    /* hidden */
-     
-    85%, 100%  {
-     
-    /* 360deg * 10 */
-     
-        transform: rotateZ(3600deg) scaleX(2.01) scaleY(2) translateZ(0);
-        box-shadow: inset 0 0 0 rgba(255, 0, 0, 0), 
-                    inset 0 0 0 rgba(252, 150, 0, 0), 
-                    inset 0 0 0 rgba(0, 255, 0, 0), 
-                    inset 0 0 0 rgba(0, 150, 255, 0);
-    }
-}
- 
-@keyframes ring  {
-    0%  {
-        transform: scaleX(0.1) scaleY(0.5);
-        box-shadow: inset 0.8em 0 0 rgba(255, 0, 0, 0.5), 
-                    inset 0 0.8em 0 rgba(252, 150, 0, 0.5), 
-                    inset -0.8em 0 0 rgba(0, 255, 0, 0.5), 
-                    inset 0 -0.8em 0 rgba(0, 150, 255, 0.5);
-    }
-     
-    /* hidden */
-     
-    75%, 100%  {
-        transform: scaleX(2) scaleY(2.1);
-        box-shadow: inset 0 0 0 rgba(255, 0, 0, 0), 
-                    inset 0 0 0 rgba(252, 150, 0, 0), 
-                    inset 0 0 0 rgba(0, 255, 0, 0), 
-                    inset 0 0 0 rgba(0, 150, 255, 0);
-    }
-}
- 
-@keyframes ring2  {
-    0%  {
-        transform: scaleX(0.5) scaleY(0.1);
-        box-shadow: inset 0.8em 0 0 rgba(255, 0, 0, 0.5), 
-                    inset 0 0.8em 0 rgba(252, 150, 0, 0.5), 
-                    inset -0.8em 0 0 rgba(0, 255, 0, 0.5), 
-                    inset 0 -0.8em 0 rgba(0, 150, 255, 0.5);
-    }
-     
-    /* hidden */
-     
-    65%, 100%  {
-        transform: scaleX(2) scaleY(2.1);
-        box-shadow: inset 0 0 0 rgba(255, 0, 0, 0), 
-                    inset 0 0 0 rgba(252, 150, 0, 0), 
-                    inset 0 0 0 rgba(0, 255, 0, 0), 
-                    inset 0 0 0 rgba(0, 150, 255, 0);
-    }
-}
+
 	  </style>
 	  <!-- JS -->
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
@@ -239,15 +141,15 @@ if ( $userraw == "" ) {
 
       //paste this code under head tag or in a seperate js file.
       // Wait for window load
-      //  $(window).load(function() {
-      //  // Animate loader off screen
-      //  $(".loading").fadeOut("slow");;
-      //});
+        $(window).load(function() {
+        // Animate loader off screen
+        $(".se-pre-con").fadeOut("slow");;
+      });
     </script>
 	</head>
   <body onload="simClickMain()">
     <!-- Pre loader -->
-    <div class="loading"><p></p><p><center><a class="external" href="loadslow.php">Loading Slow? See Why</a></center></p></div>
+    <div class="se-pre-con"><p></p><p><center><a class="external" href="loadslow.php">Loading Slow? See Why</a></center></p></div>
     <!-- If JS disabled redirect to ENABLEJS -->
 	  <noscript>
 	  	<meta http-equiv="refresh" content="0;URL='http://ichanger.tk/enable/javascript.php?no=js'" /> 
@@ -503,8 +405,8 @@ echo"Dev Mode Enabled";
               <option>Facebook</option>
               <option>Twitter</option>
               <option>Flipboard</option>
-              <option>YouTube</option>
-              <option>Instagram</option>
+              <option>YouTube</option> 
+             <option>Instagram</option>
               <option>Skype</option>
               <option>Photos</option>
               <option>Google</option>
