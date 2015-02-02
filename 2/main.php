675460ffcbcecdd976d8df5b18fe4043464a7392
Rosco1502 <thelocken@gmail.com>
2015-02-02 00:14:59 +0000

    [R]Started New Version

--- 2/main.php
+++ 2/main.php
@@ -1,56 +1,299 @@
 <!DOCTYPE html>
 <html>
-<!-- Copyright TheLocken LLC 2015 -->
-<head>
+  <head>
+    <!-- Required meta tags-->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, minimal-ui">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
-    <title>My App</title>
+    <title>iChanger</title>
     <link rel="stylesheet" href="ui/css/framework7.css">
     <link rel="stylesheet" href="ui/css/framework7.min.css">
 	<link rel="stylesheet" href="ui/css/framework7.themes.css">
 	<link rel="stylesheet" href="ui/css/f7-2.css">
-    <style>
-      .statusbar-overlay {
-        background: SkyBlue;
-      }
-    </style>
-</head>
-<body>
+  </head>
+  <body>
+    <!-- Status bar overlay for full screen mode (PhoneGap) -->
+    <div class="statusbar-overlay"></div>
+    <!-- Views -->
     <div class="views">
-        <div class="view view-main">
-            <div class="navbar">
-                <div class="navbar-inner">
-                    <div class="left"></div>
-                        <div class="center sliding">Through Navbar</div>
-                            <div class="right"></div>
-                </div>
-            </div>
-                <div class="pages navbar-through toolbar-through">
-                    <div data-page="home" class="page">
-                        <div class="page-content">
-                            <div class="content-block">
-    <p><a href="http://ichangerapp.com/custom/custom.php">About page</a></p>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse at nibh felis. Nunc consequat diam et tellus tempor gravida. Donec hendrerit aliquet risus, ut tempor purus dictum sit amet. Integer sit amet lacus eget ipsum pulvinar interdum. Proin semper turpis sed placerat dapibus. Sed iaculis id nibh a viverra. Sed vitae tellus sed purus lacinia dignissim. Aenean sagittis interdum leo in molestie. Aliquam sodales in diam eu consectetur. Sed posuere a orci id imperdiet.</p>
-    <p>Donec et nulla auctor massa pharetra adipiscing ut sit amet sem. Suspendisse molestie velit vitae mattis tincidunt. Ut sit amet quam mollis, vulputate turpis vel, sagittis felis. Vestibulum ornare ut eros vitae adipiscing. Vestibulum volutpat justo enim, ullamcorper vulputate sapien lacinia vel. Integer sed justo ultrices augue tincidunt dictum eu vel orci. Mauris sodales auctor diam vel condimentum.</p>
-    <p>Praesent mauris purus, faucibus vel hendrerit at, dapibus quis lorem. Sed placerat fermentum blandit. Suspendisse potenti. Cras sollicitudin laoreet tellus, ut gravida leo eleifend convallis. Sed pharetra nisl quis libero fermentum pharetra. Cras lacinia quam turpis, eget varius risus interdum sit amet. Quisque laoreet tortor dui, vitae accumsan lacus fringilla in. Quisque consequat placerat risus, non ornare felis scelerisque quis. Sed adipiscing diam tellus, vel faucibus mauris rhoncus vel. Vestibulum eu ultrices tortor, non suscipit lorem. Mauris tellus nulla, volutpat quis lacus eu, scelerisque adipiscing dui. Nullam nec tempor sem, nec pulvinar sapien. Etiam blandit condimentum vehicula.</p>
-    <p>Praesent nec imperdiet diam. Maecenas vel lectus porttitor, consectetur magna nec, viverra sem. Aliquam sed risus dolor. Morbi tincidunt ut libero id sodales. Integer blandit varius nisi quis consectetur. Nulla pellentesque elementum ligula vitae porta. Nunc sollicitudin mi quis mi mattis cursus. Nulla diam felis, ullamcorper eget lacinia ac, auctor id velit. Fusce enim nunc, egestas a augue vitae, malesuada tincidunt risus. Nullam fringilla, enim nec porta iaculis, enim leo pharetra nunc, eget rutrum tortor dui et risus. Etiam sit amet molestie dolor. Curabitur ultrices justo ut augue ornare, vel pharetra libero adipiscing. Duis rhoncus a felis ac venenatis. Duis posuere non leo vitae tincidunt. Integer luctus arcu ut risus posuere, vel vehicula ipsum elementum. Duis et cursus sapien. </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                    <div class="toolbar">
-                        <div class="toolbar-inner"><a href="#" class="link">Hello</a><a href="#" class="link">World</a></div>
-                    </div>
+      <!-- Your main view, should have "view-main" class -->
+      <div class="view view-main">
+        <!-- Top Navbar-->
+        <div class="navbar">
+          <div class="navbar-inner">
+            <!-- We need cool sliding animation on title element, so we have additional "sliding" class -->
+            <div class="center sliding">Awesome App</div>
+          </div>
         </div>
+        <!-- Pages container, because we use fixed-through navbar and toolbar, it has additional appropriate classes-->
+        <div class="pages navbar-through toolbar-through">
+          <!-- Page, "data-page" contains page name -->
+          <div data-page="index" class="page">
+            <!-- Scrollable page content -->
+            <div class="page-content">
+              <p>Page content goes here</p>
+              <!-- Link to another page -->
+              <a href="about.html">About app</a>
+            </div>
+          </div>
+        </div>
+        <!-- Bottom Toolbar-->
+        <div class="toolbar">
+          <div class="toolbar-inner">
+            <!-- Toolbar links -->
+            <a href="#view-2" class="link">Link 1</a>
+            <a href="#" class="link">Link 2</a>
+          </div>
+        </div>
+      </div>
+      
+            <div id="view-2" class="view tab">
+        <!-- We can make with view with navigation, let's add Top Navbar-->
+        <div class="navbar">
+          <div class="navbar-inner">
+            <div class="center sliding">iChanger - Custom</div>
+            <div class="right"><a href="#" class="open-panel link icon-only"><i class="icon icon-bars"></i></a></div>
+          </div>
+        </div>
+        <div class="pages navbar-through">
+          <div data-page="index-1" class="page">
+            <div class="page-content">
+<div class="content-block-title">Customize!</div>
+<div class="list-block">
+	<!-- 
+	
+	Start Form 
+	
+	-->
+<form onsubmit="" action="http://www.ichangerapp.com/custom/customend.php?req=custom" method="post" enctype="multipart/form-data">
+  <ul>
+    <!-- Text inputs -->
+    
+    <li>
+      <div class="item-content">
+        <div class="item-media"><i class="icon icon-custom-title"></i></div>
+        <div class="item-inner">
+          <div class="item-title label">Title</div>
+          <div class="item-input">
+            <input id="title" name="title" type="text">         </div>
+        </div>
+      </div>
+    </li>
+    <!-- Select -->
+    <li>
+      <div class="item-content">
+        <div class="item-media"><i class="icon icon-custom-app"></i></div>
+        <div class="item-inner">
+          <div class="item-title label">Open</div>
+          <div class="item-input">
+            <select id="app" name="app">
+              <option>App Store</option>
+              <option>Calendar</option>
+              <option>Facetime</option>
+              <option>GameCenter</option>
+              <option>iBooks</option>
+              <option>iTunes</option>
+              <option>Mail</option>
+              <option>Maps</option>
+              <option>Messages</option>
+              <option>Music</option>
+              <option>Passbook</option>
+              <option>Phone</option>
+              <option>Reminders</option>
+              <option>Remote</option>
+              <option>Safari</option>
+              <option>Videos</option>
+              <option>Chrome</option>
+              <option>Facebook</option>
+              <option>Twitter</option>
+              <option>Flipboard</option>
+              <option>YouTube</option>
+              <option>Instagram</option>
+              <option>Skype</option>
+              <option>Photos</option>
+              <option>Google</option>
+              <option>Google+</option>
+              <option>iChanger</option>
+              <option>URI Scheme</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </li>
+    <!-- Date -->
+    <li>
+      <div class="item-content">
+        <div class="item-media"><i class="icon icon-custom-icon"></i></div>
+        <div class="item-inner">
+          <div class="item-title label">Icon</div>
+          <div class="item-input">
+            <input name="file" id="file" type="file" accept="image/*" capture="camera" value="Choose Icon">
+          </div>
+        </div>
+      </div>
+    </li>
+    <!--              ---- FACEBOOK ----                  --->
+    <div id="FacebookFunction" style="display:none;">
+    <li>
+      <div class="item-content">
+        <div class="item-media"><i class="icon icon-custom-func"></i></div>
+        <div class="item-inner">
+          <div class="item-title label">Function</div>
+          <div class="item-input">
+            <select id="FBResult" name="func">
+              <option>Your Profile</option>
+              <option>Friends List</option>
+              <option>Notification List</option>
+              <option>News Feed</option>
+              <option>Events Feed</option>
+              <option>Requests List</option>
+              <option>Notes Page</option>
+              <option>Photo Albums</option>
+              <option>Mailbox</option>
+              <option>Messaging</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </li>
     </div>
-<script type="text/javascript" src="../dist/js/framework7.min.js"></script>
+    <!--            END FACEBOOK FUCNTIONS                         -->
+    <!--              ---- Twitter ----                  --->
+    <div id="TwitterFunction" style="display:none;">
+    <li>
+      <div class="item-content">
+        <div class="item-media"><i class="icon icon-custom-func"></i></div>
+        <div class="item-inner">
+          <div class="item-title label">Function</div>
+          <div class="item-input">
+            <select id="TWResult" name="func">
+              <option>----</option>
+              <option>Timeline</option>
+              <option>Mentions</option>
+              <option>Messages</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </li>
+    </div>
+    <!--            END TWITTER FUCNTIONS                         -->
+    <!--              ---- YouTube ----                  --->
+    <div id="YouTubeFunction" style="display:none;">
+    <li>
+      <div class="item-content">
+        <div class="item-media"><i class="icon icon-custom-func"></i></div>
+        <div class="item-inner">
+          <div class="item-title label">Function</div>
+          <div class="item-input">
+            <select id="YTResult" name="func">
+              <option>----</option>
+              <option>None</option>
+              <option>Open Video</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </li>
+    </div>
+    <!--            END YOUTUBE FUCNTIONS                         -->
+    <!--     YOUTUBE VIDEO ID IF OPEN VIDEO SELECTED              -->
+    <div id="YTVideo" style="display:none;">
+    <li>
+      <div class="item-content">
+        <div class="item-media"><i class="icon icon-custom-video"></i></div>
+        <div class="item-inner">
+          <div class="item-title label">Video ID</div>
+            <div class="item-input">
+              <input type="text" name="video" id="video" placeholder="ZZ5LpwO-An4">
+            </div>  
+        </div>
+      </div>
+    </li>
+    </div>
+    <!--           END YOUTUBE VIDEO ID                           -->
+    <!--           CUSTOM URI SCHEME ID                           -->
+    <div id="UriScheme" style="display:none;">
+    <li>
+      <div class="item-content">
+        <div class="item-media"><i class="icon icon-custom-video"></i></div>
+        <div class="item-inner">
+          <div class="item-title label">URI</div>
+            <div class="item-input">
+              <input type="text" name="customuri" id="customuri" placeholder="gamecenter://">
+            </div>  
+        </div>
+      </div>
+    </li>
+    </div>
+    <!--          END CUSTOM URI SCHEME ID                        -->
+  </ul>
+	<input type="submit" name="submit" value="Create!" class="button button-big button-round color-red">
+	</form>
+</div>
+	<div class="content-block-title">Notice</div>
+    <div class="content-block">
+        <div id="NWarning" style="display:none;"><p style="color:red"><img src="thumbs/error-50.png" alt="Warning" height="20" width="20">Warning Facebook Notification List is bugged an may not work properly.</p></div>
+        <p>Icons are deleted off our server after 10 hours of being uploaded to keep our servers happy.</p>
+        <p><a href="#" data-popup=".popup-privacy" value="Create!" class="open-popup close-panel item-link">When you use iChanger you agree to the Privacy Policy</a></p>
+    </div>
+      </div>
+      </div>
+      </div>
+      </div>
 <script>
-      var myApp = new Framework7();
-      var mainView = myApp.addView('.view', {
-        dynamicNavbar: true
-      });
-    </script>
-</body>
-</html>
+$('#app').on('change',function(){
+    if( $(this).val()==="Facebook"){
+    $("#FacebookFunction").show()
+    }
+    else{
+    $("#FacebookFunction").hide()
+    }
+});
+$('#app').on('change',function(){
+    if( $(this).val()==="Twitter"){
+    $("#TwitterFunction").show()
+    }
+    else{
+    $("#TwitterFunction").hide()
+    }
+});
+$('#app').on('change',function(){
+    if( $(this).val()==="YouTube"){
+    $("#YouTubeFunction").show()
+    }
+    else{
+    $("#YouTubeFunction").hide()
+    }
+});
+$('#app').on('change',function(){
+    if( $(this).val()==="URI Scheme"){
+    $("#UriScheme").show()
+    }
+    else{
+    $("#UriScheme").hide()
+    }
+});
+$('#FBResult').on('change',function(){
+    if( $(this).val()==="Notification List"){
+    $("#NWarning").show()
+    }
+    else{
+    $("#NWarning").hide()
+    }
+});
+$('#YTResult').on('change',function(){
+    if( $(this).val()==="Open Video"){
+    $("#YTVideo").show()
+    }
+    else{
+    $("#YTVideo").hide()
+    }
+});
+</script>
+    </div>
+    <script type="text/javascript" src="js/framework7.min.js"></script>
+    <script type="text/javascript" src="js/f7-2.js"></script>
+  </body>
+</html> 
