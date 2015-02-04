eaeb54eaae5682d8cd498e44a8ec8880225ea01e
Rosco1502 <thelocken@gmail.com>
2015-02-04 07:15:23 +0000

    [R] Cleaned Up Installer + Made 3b Compatible

--- 2/install.php
+++ 2/install.php
@@ -4,58 +4,23 @@
 |-------------------------|
 | iChanger 2 Main Handler |
  \-----------------------/
- / VERY IMPORTANT FILE!! \
-|  DO NOT EDIT W/O PERMS  |
- \_______________________/
+ */
 
-
-*/
-// Connects to your Database
-//mysql_connect("thelocken.com", "", "") 
-//	or die(mysql_error()); 
-//mysql_select_db("thelocken_test_db")
-//	or die(mysql_error());
-//Adds one to the counter 
-//mysql_query("UPDATE counter SET counter = counter + 1");
-
-//Retreives the current count 
-//$count = mysql_fetch_row(mysql_query("SELECT counter FROM counter WHERE refer=''"));
-//$ref1 = mysql_fetch_row(mysql_query("SELECT counter FROM counter WHERE refer='" . htmlspecialchars($_GET["ref"]) . "'")); 
-
-//Displays the count on your site
-//echo "<!--<p>Count=</p>";
-//print $count[0]; 
-//echo "<p>Referer=</p>";
-//print $ref1[0]; 
-//echo "<!-- Copyright TheLocken, LLC 2014 -->";
-
-//if (mysql_error()) {
-//  echo "Failed to connect to iChanger Servers! Error Code: " . mysqli_connect_error(); 
-//}
-?>
-<?php
-// Variables
-// Display Dev-Titles
-$dev = '' . htmlspecialchars($_GET["dev"]) . '';
 // http://74.125.224.174:6080/php/urlblock.php?vsys=1&cat=7509&title=streaming-media&rulename=Allow%20Student%20Content%20Filtering&uid=30687&url=http://youtube.com%2fMacy%27s
 // detect if bypass is enabled, if it is disable fowarding to computer site
 $bypass = '' . htmlspecialchars($_GET["b"]) . '';
-//if(empty($bypass)){
 if ( $bypass == "y" ) {
  $detect = "no";
 } else {
  $detect = "yes";
 }
-
 // START MAIN FOWARDING
 $ua = strtolower($_SERVER['HTTP_USER_AGENT']);
 if ( strpos($ua,"iphone") || strpos($ua,"ipad") ) {
    if ( strpos($ua,"safari") ) {
       //echo('<!--Running in safari on iPhone/iPad--><h1>To install iChanger 2 press <img src="http://ichangerapp.com/ui/simages/dl.png"> or <img src="http://ichangerapp.com/ui/simages/plus.png"> then select Add to Homescreen..');
-   } else if ( substr_count($ua, '/') === 3 && $v == "3" ) {
-     header('Location: http://www.ichangerapp.com/main.php?dev=' . htmlspecialchars($_GET["dev"]) . '&');
    } else if ( substr_count($ua, '/') === 3 ) {
-     header('Location: http://www.ichangerapp.com/mainf7.php?dev=' . htmlspecialchars($_GET["dev"]) . '&');
+     header('Location: http://www.ichangerapp.com/main.php');
    } else if ( substr_count($ua, '/') === 2 ) {
    } else if ( $bypass == "no" ) {
       header('Location: http://www.ichangerapp.com/chrome.php');
@@ -99,17 +64,13 @@ if ( strpos($ua,"iphone") || strpos($ua,"ipad") ) {
   // remove session if visit again
   	addToHomescreen.removeSession()
   </script>
-  <script language="JavaScript">
-objImage = new Image();
-objImage.src='pics/iC2-5.png';
+<script>
+if(navigator.userAgent.match('CriOS')) {
+    window.location.replace("http://www.ichangerapp.com/chrome.php");
+}
 </script>
 </head>
 	  <noscript>
 	  	<meta http-equiv="refresh" content="0;URL='http://ichangerapp.com/enable/javascript.php?no=js'" /> 
 	  </noscript>
-<script>
-if(navigator.userAgent.match('CriOS')) {
-    //document.write('<meta http-equiv="refresh" content="0;URL='http://example.com/'" />');
-    window.location.replace("http://www.ichangerapp.com/chrome.php");
-}
-</script>
+	  iChanger Installer
