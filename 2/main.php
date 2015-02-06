8721a22465e918c2d5b7ddf6751543a687429e58
Rosco1502 <thelocken@gmail.com>
2015-02-06 01:50:21 +0000

    [R]

--- 2/main.php
+++ 2/main.php
@@ -75,8 +75,8 @@
                   <div class="col-50"><a href="#" data-panel="right" class="button open-panel">Right Panel</a></div>
                 </div>
                 <div class="row">
-                  <div class="col-50">50%</div>
-                  <div class="col-50">50%</div>
+                  <div class="col-50"><center>50%</center></div>
+                  <div class="col-50"><center>50%</center></div>
                 </div>
               </div>
             </div>
