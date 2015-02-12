3b4d87c9380b249693a1b51779eb140a8bcdb0ff
Rosco1502 <thelocken@gmail.com>
2015-02-12 05:44:32 +0000

    [R]Started New Version

--- 2/mainview.php
+++ 2/mainview.php
@@ -361,6 +361,7 @@
 </div>
 </div>
 </div>
+</div>
     </div>
     <script type="text/javascript" src="js/framework7.min.js"></script>
     <script type="text/javascript" src="js/mainview.js"></script>
