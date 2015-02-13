b027201979323fa041871a948fe3f1cd06ada4c7
Rosco1502 <thelocken@gmail.com>
2015-02-13 02:40:16 +0000

    [R]Started New Version

--- 2/custom/custom.php
+++ 2/custom/custom.php
@@ -1,13 +1,15 @@
+      <div id="view-2" class="view tab">
+        <!-- We can make with view with navigation, let's add Top Navbar-->
         <div class="navbar">
           <div class="navbar-inner">
-			      <div class="left sliding"><a href="mainview.php" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>
-            <div class="center sliding">iChanger - Browse</div>
+            <div class="center sliding">iChanger - Custom</div>
             <div class="right"><a href="#" class="open-panel link icon-only"><i class="icon icon-bars"></i></a></div>
           </div>
         </div>
         <div class="pages navbar-through">
           <div data-page="index-1" class="page">
             <div class="page-content">
+<div class="content-block-title">Customize!</div>
 <div class="list-block">
 	<!-- 
 	
@@ -17,9 +19,10 @@
 <form onsubmit="" action="http://www.ichangerapp.com/custom/customend.php?req=custom" method="post" enctype="multipart/form-data">
   <ul>
     <!-- Text inputs -->
+    
     <li>
       <div class="item-content">
-        <div class="item-media"><i class="icon icon-form-name"></i></div>
+        <div class="item-media"><i class="icon icon-custom-title"></i></div>
         <div class="item-inner">
           <div class="item-title label">Title</div>
           <div class="item-input">
@@ -30,9 +33,9 @@
     <!-- Select -->
     <li>
       <div class="item-content">
-        <div class="item-media"><i class="icon icon-form-gender"></i></div>
+        <div class="item-media"><i class="icon icon-custom-app"></i></div>
         <div class="item-inner">
-          <div class="item-title label">App</div>
+          <div class="item-title label">Open</div>
           <div class="item-input">
             <select id="app" name="app">
               <option>App Store</option>
@@ -61,20 +64,8 @@
               <option>Photos</option>
               <option>Google</option>
               <option>Google+</option>
-              <?php /*<!--<option></option>
-              <option></option>
-              <option></option>
-              <option></option>
-              <option></option>
-              <option></option>
-              <option></option>
-              <option></option>
-              <option></option>
-              <option></option>
-              <option></option>
-              <option></option>
-              <option></option>
-              <option></option>-->*/ ?>
+              <option>iChanger</option>
+              <option>URI Scheme</option>
             </select>
           </div>
         </div>
@@ -83,21 +74,171 @@
     <!-- Date -->
     <li>
       <div class="item-content">
-        <div class="item-media"><i class="icon icon-form-calendar"></i></div>
+        <div class="item-media"><i class="icon icon-custom-icon"></i></div>
         <div class="item-inner">
           <div class="item-title label">Icon</div>
           <div class="item-input">
-            <input name="file" id="file" type="file" accept="image/*" capture="camera" class="list-val button picupload" value="Choose...">
+            <input name="file" id="file" type="file" accept="image/*" capture="camera" value="Choose Icon">
           </div>
         </div>
       </div>
     </li>
+    <!--              ---- FACEBOOK ----                  --->
+    <div id="FacebookFunction" style="display:none;">
+    <li>
+      <div class="item-content">
+        <div class="item-media"><i class="icon icon-custom-func"></i></div>
+        <div class="item-inner">
+          <div class="item-title label">Function</div>
+          <div class="item-input">
+            <select id="FBResult" name="func">
+              <option>Your Profile</option>
+              <option>Friends List</option>
+              <option>Notification List</option>
+              <option>News Feed</option>
+              <option>Events Feed</option>
+              <option>Requests List</option>
+              <option>Notes Page</option>
+              <option>Photo Albums</option>
+              <option>Mailbox</option>
+              <option>Messaging</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </li>
+    </div>
+    <!--            END FACEBOOK FUCNTIONS                         -->
+    <!--              ---- Twitter ----                  --->
+    <div id="TwitterFunction" style="display:none;">
+    <li>
+      <div class="item-content">
+        <div class="item-media"><i class="icon icon-custom-func"></i></div>
+        <div class="item-inner">
+          <div class="item-title label">Function</div>
+          <div class="item-input">
+            <select id="TWResult" name="func">
+              <option>----</option>
+              <option>Timeline</option>
+              <option>Mentions</option>
+              <option>Messages</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </li>
+    </div>
+    <!--            END TWITTER FUCNTIONS                         -->
+    <!--              ---- YouTube ----                  --->
+    <div id="YouTubeFunction" style="display:none;">
+    <li>
+      <div class="item-content">
+        <div class="item-media"><i class="icon icon-custom-func"></i></div>
+        <div class="item-inner">
+          <div class="item-title label">Function</div>
+          <div class="item-input">
+            <select id="YTResult" name="func">
+              <option>----</option>
+              <option>None</option>
+              <option>Open Video</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </li>
+    </div>
+    <!--            END YOUTUBE FUCNTIONS                         -->
+    <!--     YOUTUBE VIDEO ID IF OPEN VIDEO SELECTED              -->
+    <div id="YTVideo" style="display:none;">
+    <li>
+      <div class="item-content">
+        <div class="item-media"><i class="icon icon-custom-video"></i></div>
+        <div class="item-inner">
+          <div class="item-title label">Video ID</div>
+            <div class="item-input">
+              <input type="text" name="video" id="video" placeholder="ZZ5LpwO-An4">
+            </div>  
+        </div>
+      </div>
+    </li>
+    </div>
+    <!--           END YOUTUBE VIDEO ID                           -->
+    <!--           CUSTOM URI SCHEME ID                           -->
+    <div id="UriScheme" style="display:none;">
+    <li>
+      <div class="item-content">
+        <div class="item-media"><i class="icon icon-custom-video"></i></div>
+        <div class="item-inner">
+          <div class="item-title label">URI</div>
+            <div class="item-input">
+              <input type="text" name="customuri" id="customuri" placeholder="gamecenter://">
+            </div>  
+        </div>
+      </div>
+    </li>
+    </div>
+    <!--          END CUSTOM URI SCHEME ID                        -->
   </ul>
-	<input type="submit" name="submit" class="button">
+	<input type="submit" name="submit" value="Create!" class="button button-big button-round color-red">
 	</form>
 </div>
 	<div class="content-block-title">Notice</div>
     <div class="content-block">
-        <p>Icons are deleted off our server after a few days of being uploaded to keep our servers happy.</p>
+        <div id="NWarning" style="display:none;"><p style="color:red"><img src="thumbs/error-50.png" alt="Warning" height="20" width="20">Warning Facebook Notification List is bugged an may not work properly.</p></div>
+        <p>Icons are deleted off our server after 10 hours of being uploaded to keep our servers happy.</p>
         <p><a href="#" data-popup=".popup-privacy" value="Create!" class="open-popup close-panel item-link">When you use iChanger you agree to the Privacy Policy</a></p>
     </div>
+      </div>
+      </div>
+      </div>
+      </div>
+<script>
+$('#app').on('change',function(){
+    if( $(this).val()==="Facebook"){
+    $("#FacebookFunction").show()
+    }
+    else{
+    $("#FacebookFunction").hide()
+    }
+});
+$('#app').on('change',function(){
+    if( $(this).val()==="Twitter"){
+    $("#TwitterFunction").show()
+    }
+    else{
+    $("#TwitterFunction").hide()
+    }
+});
+$('#app').on('change',function(){
+    if( $(this).val()==="YouTube"){
+    $("#YouTubeFunction").show()
+    }
+    else{
+    $("#YouTubeFunction").hide()
+    }
+});
+$('#app').on('change',function(){
+    if( $(this).val()==="URI Scheme"){
+    $("#UriScheme").show()
+    }
+    else{
+    $("#UriScheme").hide()
+    }
+});
+$('#FBResult').on('change',function(){
+    if( $(this).val()==="Notification List"){
+    $("#NWarning").show()
+    }
+    else{
+    $("#NWarning").hide()
+    }
+});
+$('#YTResult').on('change',function(){
+    if( $(this).val()==="Open Video"){
+    $("#YTVideo").show()
+    }
+    else{
+    $("#YTVideo").hide()
+    }
+});
+</script>
