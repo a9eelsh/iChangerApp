2a640dad7b3b2f1a8eecf24c4584ed449cb1302c
Rosco1502 <thelocken@gmail.com>
2015-02-18 03:49:24 +0000

    [R]

--- 2/appStoref7.php
+++ 2/appStoref7.php
@@ -1,7 +1,7 @@
         <div class="navbar">
           <div class="navbar-inner">
 			      <div class="left sliding"><a href="mainf7.php<?php print $uend; ?>" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>
-            <div class="center sliding">iChanger 2 - App Store</div>
+            <div class="center sliding">App Store</div>
             <div class="right"><a href="#" class="open-panel link icon-only"><i class="icon icon-bars"></i></a></div>
           </div>
         </div>
