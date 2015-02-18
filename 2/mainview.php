3d123523d48b4ef0d5c85942b500d911a86c79d8
Rosco1502 <thelocken@gmail.com>
2015-02-18 01:05:27 +0000

    [R]

--- 2/mainview.php
+++ 2/mainview.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <meta charset="utf-8">
+    <meta charset="utf-8">-
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, minimal-ui">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
@@ -19,7 +19,7 @@
   var input = $(this),
       numFiles = input.get(0).files ? input.get(0).files.length : 1,
       label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
-  input.trigger('fileselect', [numFiles, label]);
+  input.trigger('fileselect', [numFiles, label]); b
 });
 
 $(document).ready( function() {
