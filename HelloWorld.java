0ca4a98f9500d0999026b6a7f1482cf445ab913b
Rosco1502 <thelocken@gmail.com>
2015-02-20 01:35:55 +0000

    [R]

--- HelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld {
-
-    public static void main(String[] args) {
-        System.out.println("This is how CLASS files run on the Virtual Machine Were on right now.");
-    }
-
-}
