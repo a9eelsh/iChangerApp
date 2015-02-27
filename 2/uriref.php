2e4ec4bd95b0423612561074daf892697b47355e
Rosco1502 <thelocken@gmail.com>
2015-02-27 07:44:45 +0000

    [R] Sped up and simplified redirections

--- 2/uriref.php
+++ 2/uriref.php
@@ -1,8 +1,4 @@
-<!-- COPYRIGHT 2014 TheLocken LLC All Rights Reserved + DMCA Rights -->
 <?php
-// This file is here to set the HTTP_REF in urigen.php/uridata.php for improved dectection and
-// convert photos to URI Data Photos
-
 $appname = '' . htmlspecialchars($_GET["n"]) . '';
 $iconfolder = '' . htmlspecialchars($_GET["a"]) . '';
 $iconcdir = '' . htmlspecialchars($_GET["c"]) . '';
@@ -11,18 +7,11 @@ $iconext = '' . htmlspecialchars($_GET["pe"]) . '';
 $appurl = '' . htmlspecialchars($_GET["ur"]) . '';
 $v = '' . htmlspecialchars($_GET["v"]) . '';
 $gplus = '' . htmlspecialchars($_GET["gplus"]) . '';
+$switch = '' . htmlspecialchars($_GET["switch"]) . '';
+$url = "uridata.php";
 if ( $gplus == "yes" ) {
 	$gplusurl = "%2B";
 }
-$switch = '' . htmlspecialchars($_GET["switch"]) . '';
-
-// Start Switch
-//if ( $switch == "yes" ) {
-//    $url = "urigen.php";
-//} else {
-    $url = "uridata.php";
-//}
-
 header('Location: '.$url.'?n='.$appname.'&a='.$iconfolder.'&p='.$iconmain.'&pe='.$iconext.'&ur='.$appurl.'&v='.$v.'&gplus='.$gplus.'&c='.$iconcdir.'', true, 301);
 exit;
 ?>
