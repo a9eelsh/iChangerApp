8522a13f87a6b55566f449ea8f58e7a4e11de536
Rosco1502 <thelocken@gmail.com>
2015-02-28 07:09:02 +0000

    [R]

--- 2/custom/customend.php
+++ 2/custom/customend.php
@@ -13,16 +13,21 @@ if ((($_FILES["file"]["type"] == "image/gif")
 || ($_FILES["file"]["type"] == "image/png"))
 && ($_FILES["file"]["size"] < 2000000)
 && in_array($extension, $allowedExts)) {
+  $result = "Passed EXTENSION VERI";
   if ($_FILES["file"]["error"] > 0) {
+    $result = "Passed ERROR";
     echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
   } else {
+    $result = "Passed UPLOAD";
     echo "Upload: " . $_FILES["file"]["name"] . "<br>";
     echo "Type: " . $_FILES["file"]["type"] . "<br>";
     echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
     echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
     if (file_exists("$rand/" . $_FILES["file"]["name"])) {
+      $result = "Passed error file exists";
       echo $_FILES["file"]["name"] . " already exists. ";
     } else {
+      $result = "Passed move file";
       move_uploaded_file($_FILES["file"]["tmp_name"],
       "upload/" . $_FILES["file"]["name"]);
       echo "Stored in: " . "$rand/" . $_FILES["file"]["name"];
@@ -30,6 +35,7 @@ if ((($_FILES["file"]["type"] == "image/gif")
   }
 } else {
 	echo "FAIL";
+	print $result;
 }
 
 
@@ -98,64 +104,6 @@ if (file_exists($rand)) {
 
 
 
-$myfile = fopen("latest_ref.txt", "w") 
-	// echo error if failed to open latest_ref.txt
-	or die("<p>Error!</p><p>Error Code: HTTP_REF_LATESTREF_OPEN_ERROR</p>");
-// HTTP_REFERER
-$after = "\n";
-$terror = $_FILES["file"]["error"];
-$terrorl = "Return Code:\n";
-fwrite($myfile, $terrorl);
-fwrite($myfile, $terror);
-fwrite($myfile, $after);
-// USER_NAME
-$tname = $_FILES["file"]["name"];
-$tnamel = "FILE_NAME:\n";
-fwrite($myfile, $tnamel);
-fwrite($myfile, $tname);
-fwrite($myfile, $after);
-// USER_DISPLAY
-$ttype = $_FILES["file"]["type"];
-$ttypel = "FILE_TYPE:\n";
-fwrite($myfile, $ttypel);
-fwrite($myfile, $ttype);
-fwrite($myfile, $after);
-// LOCAL_IP
-$ttemp = $_FILES["file"]["tmp_name"];
-$ttempl = "TEMP_FILE:\n";
-fwrite($myfile, $ttempl);
-fwrite($myfile, $ttemp);
-fwrite($myfile, $after);
-// PROXY_IP
-$proxyip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-$proxyipl = "PROXY IP:\n";
-fwrite($myfile, $proxyipl);
-fwrite($myfile, $proxyip);
-fwrite($myfile, $after);
-// IN WEBAPP?
-$trand = "$rand";
-$trandl = "RANDOM:\n";
-fwrite($myfile, $trandl);
-fwrite($myfile, $trand);
-fwrite($myfile, $after);
-// VIEWER
-//$viewerl = "BROWSER_VIEWER:\n";
-//fwrite($myfile, $viewerl);
-//fwrite($myfile, $viewer);
-//fwrite($myfile, $after);
-// ADD TIMESTAMP
-   // Set default TimeLocation to Pacific Time
-date_default_timezone_set("America/Los_Angeles");
-$timel = "PACIFIC TIME:\n";
-$date = date("F j, Y, g:i a", floor(1341111034380/1000));
-$time = time();
-fwrite($myfile, $timel);
-fwrite($myfile, $date);
-fwrite($myfile, $after);
-fwrite($myfile, $time);
-fclose($myfile);
-?>
-<?php
 
 if ( $requestraw == "ipeep" ) {
 	$request = "Custom iPeep";
