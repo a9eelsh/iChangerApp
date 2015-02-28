962981fc7a0f6b5d9c19b187fcca91a3f6f69001
Rosco1502 <thelocken@gmail.com>
2015-02-28 02:50:02 +0000

    [R]

--- 2/uriprofile.php
+++ 2/uriprofile.php
@@ -17,11 +17,7 @@ $UUID2 =  rand;
 
 $image = 'http://ichangerapp.com/' . htmlspecialchars($iconfolder) . '/' . htmlspecialchars($iconmain) .'' . htmlspecialchars($iconext) . '';
 $imageData = base64_encode(file_get_contents($image));
-
-print $image;
-print $imageData;
 ?>
-
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
