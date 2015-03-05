d8a082174b90e28afbac0e453e5717d94028e8b5
Rosco1502 <thelocken@gmail.com>
2015-03-05 04:58:23 +0000

    [R]

--- 2/uriprofile.php
+++ 2/uriprofile.php
@@ -24,55 +24,58 @@ $UUID2 =  rand;
 $image = file_get_contents('http://ichangerapp.com/' . htmlspecialchars($iconfolder) . '/' . htmlspecialchars($iconmain) .'' . htmlspecialchars($iconext) . '');
 $imageData = base64_encode($image);
 
-header('Content-type: text/xml');
+$mobileconfig .= '<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">';
+$mobileconfig .= '<plist version="1.0">';
+$mobileconfig .= "<dict>";
+$mobileconfig .= "	<key>PayloadContent</key>";
+$mobileconfig .= "	<array>";
+$mobileconfig .= "		<dict>";
+$mobileconfig .= "			<key>FullScreen</key>";
+$mobileconfig .= "			<true/>";
+$mobileconfig .= "			<key>Icon</key>";
+$mobileconfig .= "			</data>".$imageData."</data>";
+$mobileconfig .= "			<key>IsRemovable</key>";
+$mobileconfig .= "			<true/>";
+$mobileconfig .= "			<key>Label</key>";
+$mobileconfig .= "			<string>".$appname."</string>";
+$mobileconfig .= "			<key>PayloadDescription</key>";
+$mobileconfig .= "			<string>Configures settings for a Web Clip</string>";
+$mobileconfig .= "			<key>PayloadDisplayName</key>";
+$mobileconfig .= "			<string>".$appname."</string>";
+$mobileconfig .= "			<key>PayloadIdentifier</key>";
+$mobileconfig .= "			<string>".$UUID2."</string>";
+$mobileconfig .= "			<key>PayloadType</key>";
+$mobileconfig .= "			<string>com.apple.webClip.managed</string>";
+$mobileconfig .= "			<key>PayloadUUID</key>";
+$mobileconfig .= "			<string>".$UUID."</string>";
+$mobileconfig .= "			<key>PayloadVersion</key>";
+$mobileconfig .= "			<real>1</real>";
+$mobileconfig .= "			<key>Precomposed</key>";
+$mobileconfig .= "			<true/>";
+$mobileconfig .= "			<key>URL</key>";
+$mobileconfig .= "			<string>".$appurl."</string>";
+$mobileconfig .= "		</dict>";
+$mobileconfig .= "	</array>";
+$mobileconfig .= "	<key>PayloadDescription</key>";
+$mobileconfig .= "	<string>iChanger Profile WIP: Shortcut for ".$appname.". This function is still a Work-In-Progress and is highly experimental if you find any problems please contact us from the menu in iChanger.</string>";
+$mobileconfig .= "	<key>PayloadDisplayName</key>";
+$mobileconfig .= "	<string>iChanger Profile for ".$appname."</string>";
+$mobileconfig .= "	<key>PayloadIdentifier</key>";
+$mobileconfig .= "	<string>com.thelocken.ichanger.".$UUID."</string>";
+$mobileconfig .= "	<key>PayloadOrganization</key>";
+$mobileconfig .= "	<string>TheLocken LLC; iChanger</string>";
+$mobileconfig .= "	<key>PayloadRemovalDisallowed</key>";
+$mobileconfig .= "	<false/>";
+$mobileconfig .= "	<key>PayloadType</key>";
+$mobileconfig .= "	<string>Configuration</string>";
+$mobileconfig .= "	<key>PayloadUUID</key>";
+$mobileconfig .= "	<string>".$UUID."</string>";
+$mobileconfig .= "	<key>PayloadVersion</key>";
+$mobileconfig .= "	<integer>1</integer>";
+$mobileconfig .= "</dict>";
+$mobileconfig .= "</plist>";
+
+header('Content-type: application/x-apple-aspen-config; chatset=utf-8');
+header('Content-Disposition: attachment; filename="iChangerProfile.mobileconfig"');
+echo $mobileconfig;
 ?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-	<key>PayloadContent</key>
-	<array>
-		<dict>
-			<key>FullScreen</key>
-			<true/>
-			<key>Icon</key>
-			</data><?php print $imageData; ?></data>
-			<key>IsRemovable</key>
-			<true/>
-			<key>Label</key>
-			<string><?php print $appname; ?></string>
-			<key>PayloadDescription</key>
-			<string>Configures settings for a Web Clip</string>
-			<key>PayloadDisplayName</key>
-			<string><?php print $appname; ?></string>
-			<key>PayloadIdentifier</key>
-			<string><?php print $UUID2; ?></string>
-			<key>PayloadType</key>
-			<string>com.apple.webClip.managed</string>
-			<key>PayloadUUID</key>
-			<string><?php print $UUID; ?></string>
-			<key>PayloadVersion</key>
-			<real>1</real>
-			<key>Precomposed</key>
-			<true/>
-			<key>URL</key>
-			<string><?php print $appurl; ?></string>
-		</dict>
-	</array>
-	<key>PayloadDescription</key>
-	<string>iChanger Profile WIP: Shortcut for <?php print $appname; ?>. This function is still a Work-In-Progress and is highly experimental if you find any problems please contact us from the menu in iChanger.</string>
-	<key>PayloadDisplayName</key>
-	<string>iChanger Profile for <?php print $appname; ?></string>
-	<key>PayloadIdentifier</key>
-	<string>com.thelocken.ichanger.<?php print $UUID; ?></string>
-	<key>PayloadOrganization</key>
-	<string>TheLocken LLC; iChanger</string>
-	<key>PayloadRemovalDisallowed</key>
-	<false/>
-	<key>PayloadType</key>
-	<string>Configuration</string>
-	<key>PayloadUUID</key>
-	<string><?php print $UUID; ?></string>
-	<key>PayloadVersion</key>
-	<integer>1</integer>
-</dict>
-</plist>
