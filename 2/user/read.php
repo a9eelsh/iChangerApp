b1634eba7089608b2a8b4b5d1c12867faa3237df
Rosco1502 <thelocken@gmail.com>
2015-03-06 02:38:10 +0000

    [R]

--- 2/user/read.php
+++ 2/user/read.php
@@ -1,6 +1,7 @@
 <?php
+$UserID = '' . htmlspecialchars($_GET["id"]) . '';
 // copy file content into a string var
-$json_file = file_get_contents('posts.json');
+$json_file = file_get_contents(''.$UserID.'.json');
 // convert the string to a json object
 $jfo = json_decode($json_file);
 // read the title value
