613d94f9279b745ddb77a8c0eccd80b7aa29cdbd
Rosco1502 <thelocken@gmail.com>
2015-03-06 03:15:18 +0000

    [R]

--- 2/user/read.php
+++ 2/user/read.php
@@ -1,4 +1,5 @@
 <?php
+// load data
 $UserID = '' . htmlspecialchars($_GET["id"]) . '';
 
 $url = ''.$UserID.'.json';
@@ -9,11 +10,23 @@ $object = json_decode($json);
 
 $array = json_decode($json, TRUE);
 
-echo $object->whatever;
+// pull data
+
+$value1 = $object->Email;
+$value2 = $object['Email'];
+
+$value3 = $array->Email;
+$value4 = $array['Email'];
 ?>
-v5
+v6
+<p>LOAD DATA</p>
 <p>userid <?php print $UserID; ?></p>
 <p>url <?php print $url; ?></p>
 <p>json <?php print $json; ?></p>
 <p>object <?php print $object; ?></p>
 <p>array <?php print $array; ?></p>
+<p>PULL DATA</p>
+<p>value1 <?php print $value1; ?></p>
+<p>value2 <?php print $value2; ?></p>
+<p>value3 <?php print $value3; ?></p>
+<p>value4 <?php print $value4; ?></p>
