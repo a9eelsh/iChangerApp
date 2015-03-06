7d0c096e292c67144b52b81ddda1d5cc5ec9cb8c
Rosco1502 <thelocken@gmail.com>
2015-03-06 03:40:11 +0000

    [R]

--- 2/user/read2.php
+++ 2/user/read2.php
@@ -1,6 +1,7 @@
 <?php
     $api_url = "test.json";
     $response = file_get_contents($api_url);
-    $my_array = json_decode($response, true);   
     $my_array = json_decode($response);
     $my_array->places->place[0]->woe_name;
+    print $my_array->places->place[0]->woe_name;
+    echo $my_array->places->place[0]->woe_name;
