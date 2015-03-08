6a2ec43a416d735c47235ad1b7e3e7bb9cc1e72f
Rosco1502 <thelocken@gmail.com>
2015-03-08 08:11:40 +0000

    [R] Updated Framework

--- 2/user/read2.php
+++ 2/user/read2.php
@@ -1,7 +1,12 @@
 <?php
 $array = json_decode('{"id":1,"name":"foo","email":"foo@test.com"}', true);
+$url = htmlspecialchars($_GET['id']);
+$userarray = json_decode(''.$url.'', true);
 //$array['id'] == 1
 //$array['name'] == "foo"
 //$array['email'] == "foo@test.com"
 print $array['id'];
+echo "\n";
 print $array['email'];
+echo "\n";
+print $userarray['UserID'];
