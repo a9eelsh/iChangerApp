c33ae1e98528214d2bfe212b330851965eacf515
Rosco1502 <thelocken@gmail.com>
2015-03-08 08:47:46 +0000

    [R] Updated Framework

--- 2/user/read2.php
+++ 2/user/read2.php
@@ -1,13 +1,5 @@
 <?php
-$url = 'test.json';
-$JSON = file_get_contents($url);
-
-// echo the JSON (you can echo this to JavaScript to use it there)
-echo $JSON;
-
-// You can decode it to process it in PHP
-$data = json_decode($JSON);
-var_dump($data);
-echo "\n";
+$content=file_get_contents("test.json");
+$data=json_decode($content);
 print $data['UserID'];
 ?>
