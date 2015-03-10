39fa4e42e0342aebcd5c862d876e31b3efd64a76
Rosco1502 <thelocken@gmail.com>
2015-03-10 00:00:47 +0000

    [R] Updated Framework

--- 2/user/read2.php
+++ 2/user/read2.php
@@ -1,5 +1,6 @@
 <?php
-$strjson = '{"url": "http://coursesweb.net/", "category": "Web Development","nr_pages": 400}';
+$strjson1 = '{"url": "http://coursesweb.net/", "category": "Web Development","nr_pages": 400}';
+$strjson = 'http://ichangerapp.com/user/test.json';
 $objson = json_decode($strjson);
 $url = $objson->url;
 echo '<br/>'. $url;
