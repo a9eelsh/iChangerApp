621fd11408e26c4ab273f680946132f96cc5d152
Rosco1502 <thelocken@gmail.com>
2015-03-10 00:13:38 +0000

    [R] Updated Framework

--- 2/user/read2.php
+++ 2/user/read2.php
@@ -1,7 +1,5 @@
 <?php
-$variable = file_get_contents('http://ichangerapp.com/user/test.json');
-$decoded = json_decode($variable);
-print $variable;
-print $decoded;
-?>
-1
+$xml = new SimpleXMLElement(file_get_contents("http://ichangerapp.com/user/test.xml"));
+$UserID = $xml->UserID;
+$username = $xml->username;
+echo "UserID: $UserID<br/>username: $username";
