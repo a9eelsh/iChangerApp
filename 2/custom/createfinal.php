a55c04a825b5badc5e42f28c235686e58549b65a
Rosco1502 <thelocken@gmail.com>
2015-03-12 01:20:06 +0000

    [R]

--- 2/custom/createfinal.php
+++ 2/custom/createfinal.php
@@ -281,6 +281,8 @@ if ( $func == "Open Video" ) {
 if ( $app == "YouTube" && $func == "None" ) {
   $urls = 'https://www.youtube.com/';
 }
+
+$shortenedurl = file_get_contents('http://ichangerapp.com/s/shorten.php?longurl=' . urlencode('http://ichangerapp.com/uriref.php?a='.$endfolder.'&p='.$endpicture.'&n='.$_POST["title"].'&c=custom&ur='.$urls.''));
 ?>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, minimal-ui">
     <link rel="stylesheet" href="../ui/css/framework7.min.css">
@@ -322,8 +324,20 @@ if ( $app == "YouTube" && $func == "None" ) {
                   </li>
                 </ul>
               </div>
-              <div class="content-block">
-                <p><?php echo $result; ?></p>
+              <div class="list-block">
+                <ul>
+                  <li>
+                  <div class="item-content">
+                      <div class="item-media"><i class="icon icon-form-url"></i></div>
+                      <div class="item-inner">
+                        <div class="item-title label">Share</div>
+                        <div class="item-input">
+                          <input type="text" value="<?php print $shortenedurl; ?>">
+                        </div>
+                      </div>
+                    </div>
+                  </li>
+                </ul>
               </div>
             </div>
           </div>
