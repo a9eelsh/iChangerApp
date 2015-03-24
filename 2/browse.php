2f45464b41e8961728573520c9e36e9419ea6b54
Rosco1502 <thelocken@gmail.com>
2015-03-24 05:50:03 +0000

    [R]

--- 2/browse.php
+++ 2/browse.php
@@ -66,7 +66,7 @@
 							<span class="list-val button ">Theme</span>
                         </div>
                       </div></a></li>
-                  <li><a href="swipe-delete.html<?php print $uend; ?>" class="item-link">
+                  <li><a href="mapsf7.php" class="item-link">
                       <div class="item-content">
                         <div class="item-media"><i class="icon icon-app-maps"></i></div>
                         <div class="item-inner">
