25347f7a16fdac7b8ab496d6d945047fbb256433
Rosco1502 <thelocken@gmail.com>
2015-03-24 05:31:41 +0000

    [R]

---
+++ 2/itunesf7.php
@@ -0,0 +1,139 @@
+        <div class="navbar">
+          <div class="navbar-inner">
+			      <div class="left sliding"><a href="mainview.php" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>
+            <div class="center sliding">iTunes</div>
+            <div class="right"><a href="#" class="open-panel link icon-only"><i class="icon icon-bars"></i></a></div>
+          </div>
+        </div>
+        <div class="pages navbar-through">
+          <div data-page="index-1" class="page">
+            <div class="page-content">
+		<div class="list-block media-list">
+			<ul>
+				<li>
+					<a href="" class="already-istalled open-popover link item-link item-content">
+						<div class="item-media">
+							<img src="http://ichangerapp.com/iTunes/it-default.png" width="80">
+						</div>
+						<div class="item-inner">
+							<div class="item-title-row">
+								<div class="item-title">Default</div>
+								<div class="item-after">Installed</div>
+							</div>
+							<div class="item-subtitle">Apple</div>
+							<div class="item-text">Apple's Default AppStore Icon. This icon is here to show you the side by side comparison</div>
+						</div>
+					</a>
+				</li>
+				
+			<!--</ul>
+			<ul>-->
+				<li>
+					<a href="http://ichangerapp.com/uriref.php?a=iTunes&p=it-metro.png&n=iTunes&ur=itms://" class="external item-link item-content">
+						<div class="item-media">
+							<img src="http://ichangerapp.com/iTunes/it-metro.png" width="80">
+						</div>
+						<div class="item-inner">
+							<div class="item-title-row">
+								<div class="item-title">Metro Google</div>
+								<div class="item-after">Install</div>
+							</div>
+							<div class="item-subtitle">igh0zt</div>
+							<div class="item-text"></div>
+						</div>
+					</a>
+				</li>
+				
+			<!--</ul>
+          
+          
+          
+          
+          
+          
+          
+			<ul>-->
+				<li>
+					<a href="http://ichangerapp.com/uriref.php?a=iTunes&p=it-shad.png&n=iTunes&ur=itms://" class="external item-link item-content">
+						<div class="item-media">
+							<img src="http://ichangerapp.com/iTunes/it-shad.png" width="80">
+						</div>
+						<div class="item-inner">
+							<div class="item-title-row">
+								<div class="item-title">Long Shadow</div>
+								<div class="item-after">Install</div>
+							</div>
+							<div class="item-subtitle">PelFusion</div>
+							<div class="item-text"></div>
+						</div>
+					</a>
+				</li>
+				
+			<!--</ul>
+            <ul>-->
+          
+				<li>
+					<a href="http://ichangerapp.com/uriref.php?a=iTunes&p=it-alt.png&n=iTunes&ur=itms://" class="external item-link item-content">
+						<div class="item-media">
+							<img src="http://ichangerapp.com/iTunes/it-alt.png" width="80">
+						</div>
+						<div class="item-inner">
+							<div class="item-title-row">
+								<div class="item-title">iOS 7 Alternative</div>
+								<div class="item-after">Install</div>
+							</div>
+							<div class="item-subtitle">dtafalonso</div>
+							<div class="item-text"></div>
+						</div>
+					</a>
+				</li>
+              
+              
+              
+              
+              
+              
+              
+				<li>
+					<a href="http://ichangerapp.com/uriref.php?a=iTunes&p=it-rs.png&n=iTunes&ur=itms://" class="external item-link item-content">
+						<div class="item-media">
+							<img src="http://ichangerapp.com/iTunes/it-rs.png" width="80">
+						</div>
+						<div class="item-inner">
+							<div class="item-title-row">
+								<div class="item-title">Realistically Soft</div>
+								<div class="item-after">Install</div>
+							</div>
+							<div class="item-subtitle">iTunes</div>
+							<div class="item-text"></div>
+						</div>
+					</a>
+				</li>
+				
+              
+              
+              
+              
+              
+				<li>
+					<a href="http://ichangerapp.com/uriref.php?a=iTunes&p=it-reverse.png&n=iTunes&ur=itms://" class="external item-link item-content">
+						<div class="item-media">
+							<img src="http://ichangerapp.com/iTunes/it-reverse.png" width="80">
+						</div>
+						<div class="item-inner">
+							<div class="item-title-row">
+								<div class="item-title">Reverse</div>
+								<div class="item-after">Install</div>
+							</div>
+							<div class="item-subtitle">iTunes</div>
+							<div class="item-text"></div>
+						</div>
+					</a>
+				</li>
+              
+              
+				
+		</div>
+
+	</div>
+</div>
