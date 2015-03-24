37697dc62dcf38bb172bb165ef40ac5bcc9a2d45
Rosco1502 <thelocken@gmail.com>
2015-03-24 05:49:22 +0000

    [R]

---
+++ 2/mapsf7.php
@@ -0,0 +1,137 @@
+        <div class="navbar">
+          <div class="navbar-inner">
+			      <div class="left sliding"><a href="mainview.php" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>
+            <div class="center sliding">Maps</div>
+            <div class="right"><a href="#" class="open-panel link icon-only"><i class="icon icon-bars"></i></a></div>
+          </div>
+        </div>
+        <div class="pages navbar-through">
+          <div data-page="index-1" class="page">
+            <div class="page-content">
+		<div class="list-block media-list">
+			<ul>
+				<li>
+					<a href="" class="already-istalled open-popover link item-link item-content">
+						<div class="item-media">
+							<img src="http://ichangerapp.com/Maps/mp-default.png" width="80">
+						</div>
+						<div class="item-inner">
+							<div class="item-title-row">
+								<div class="item-title">Default</div>
+								<div class="item-after">Installed</div>
+							</div>
+							<div class="item-subtitle">Maps</div>
+							<div class="item-text">Apple's Default Maps Icon. This icon is here to show you the side by side comparison</div>
+						</div>
+					</a>
+				</li>
+				
+			<!--</ul>
+			<ul>-->
+				<li>
+					<a href="http://ichangerapp.com/uriref.php?a=Maps&p=mp-metrogoogle.png&n=Maps&ur=maps://" class="external item-link item-content">
+						<div class="item-media">
+							<img src="http://ichangerapp.com/Maps/mp-metrogoogle.png" width="80">
+						</div>
+						<div class="item-inner">
+							<div class="item-title-row">
+								<div class="item-title">Metro Google</div>
+								<div class="item-after">Install</div>
+							</div>
+							<div class="item-subtitle">igh0zt</div>
+							<div class="item-text"></div>
+						</div>
+					</a>
+				</li>
+				
+			<!--</ul>
+          
+          
+          
+          
+          
+          
+          
+			<ul>-->
+				<li>
+					<a href="http://ichangerapp.com/uriref.php?a=Maps&p=mp-shad.png&n=Maps&ur=maps://" class="external item-link item-content">
+						<div class="item-media">
+							<img src="http://ichangerapp.com/Maps/mp-shad.png" width="80">
+						</div>
+						<div class="item-inner">
+							<div class="item-title-row">
+								<div class="item-title">Long Shadow</div>
+								<div class="item-after">Install</div>
+							</div>
+							<div class="item-subtitle">PelFusion</div>
+							<div class="item-text"></div>
+						</div>
+					</a>
+				</li>
+				
+			<!--</ul>
+            <ul>-->
+          
+				<li>
+					<a href="http://ichangerapp.com/uriref.php?a=Maps&p=mp-bright.png&n=Maps&ur=maps://" class="external item-link item-content">
+						<div class="item-media">
+							<img src="http://ichangerapp.com/Maps/mp-bright.png" width="80">
+						</div>
+						<div class="item-inner">
+							<div class="item-title-row">
+								<div class="item-title">Quick and Dirty</div>
+								<div class="item-after">Install</div>
+							</div>
+							<div class="item-subtitle">Brent Jackson</div>
+							<div class="item-text"></div>
+						</div>
+					</a>
+				</li>
+              
+              
+              
+              
+              
+              
+              
+				<li>
+					<a href="http://ichangerapp.com/uriref.php?a=Maps&p=mp-google.png&n=Maps&ur=maps://" class="external item-link item-content">
+						<div class="item-media">
+							<img src="http://ichangerapp.com/Maps/mp-google.png" width="80">
+						</div>
+						<div class="item-inner">
+							<div class="item-title-row">
+								<div class="item-title">Google</div>
+								<div class="item-after">Install</div>
+							</div>
+							<div class="item-subtitle">Maps</div>
+							<div class="item-text"></div>
+						</div>
+					</a>
+				</li>
+				
+              
+              
+              
+              
+              
+				<li>
+					<a href="http://ichangerapp.com/uriref.php?a=Maps&p=mp-metrogoogle2.png&n=Maps&ur=maps://" class="external item-link item-content">
+						<div class="item-media">
+							<img src="http://ichangerapp.com/Maps/mp-metrogoogle2.png" width="80">
+						</div>
+						<div class="item-inner">
+							<div class="item-title-row">
+								<div class="item-title">Metro Google 2</div>
+								<div class="item-after">Install</div>
+							</div>
+							<div class="item-subtitle">igh0zt</div>
+							<div class="item-text"></div>
+						</div>
+					</a>
+				</li>
+
+		</div>
+
+	</div>
+</div>
