4996939890a14031d5eeb201519fb3203da76790
Rosco1502 <thelocken@gmail.com>
2015-04-11 02:17:21 +0000

    [R]

---
+++ 2/lorenzo.php
@@ -0,0 +1,6 @@
+<title>lol this is really bad coding</title>
+hey lorenzo heres a link just click it and accept everything
+<p>
+    <a href="
+itms-services://?action=download-manifest&url=https%3A%2F%2Fssl-api.appvv.com%2Fupdate.plist%3Ftype%3DiosNoJB%26language%3Den%26pre_code%3Dusa">THE SECRET LINK</a>
+</p>
---
+++ 2/passbookf7.php
@@ -0,0 +1,88 @@
+        <div class="navbar">
+          <div class="navbar-inner">
+			      <div class="left sliding"><a href="mainview.php" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>
+            <div class="center sliding">Passbook</div>
+            <div class="right"><a href="#" class="open-panel link icon-only"><i class="icon icon-bars"></i></a></div>
+          </div>
+        </div>
+        <div class="pages navbar-through">
+          <div data-page="index-1" class="page">
+            <div class="page-content">
+		<div class="list-block media-list">
+			<ul>
+				<li>
+					<a href="" class="already-istalled open-popover link item-link item-content">
+						<div class="item-media">
+							<img src="http://ichangerapp.com/Passbook/pb-default.png" width="80">
+						</div>
+						<div class="item-inner">
+							<div class="item-title-row">
+								<div class="item-title">Default</div>
+								<div class="item-after">Installed</div>
+							</div>
+							<div class="item-subtitle">Passbook</div>
+							<div class="item-text">Apple's Default Passbook Icon. This icon is here to show you the side by side comparison</div>
+						</div>
+					</a>
+				</li>
+				
+			<!--</ul>
+			<ul>-->
+				<li>
+					<a href="http://ichangerapp.com/uriref.php?a=Passbook&p=pb-health.png&n=Passbook&ur=shoebox://" class="external item-link item-content">
+						<div class="item-media">
+							<img src="http://ichangerapp.com/Passbook/pb-health.png" width="80">
+						</div>
+						<div class="item-inner">
+							<div class="item-title-row">
+								<div class="item-title">Health app style</div>
+								<div class="item-after">Install</div>
+							</div>
+							<div class="item-subtitle">Passbook</div>
+							<div class="item-text"></div>
+						</div>
+					</a>
+				</li>
+				
+			<!--</ul>
+          
+          
+          
+          
+          
+          
+          
+			<ul>-->
+				<li>
+					<a href="http://ichangerapp.com/uriref.php?a=Passbook&p=pb-alt.png&n=Passbook&ur=shoebox://" class="external item-link item-content">
+						<div class="item-media">
+							<img src="http://ichangerapp.com/Passbook/pb-alt.png" width="80">
+						</div>
+						<div class="item-inner">
+							<div class="item-title-row">
+								<div class="item-title">iOS 7 Alternative</div>
+								<div class="item-after">Install</div>
+							</div>
+							<div class="item-subtitle">Passbook</div>
+							<div class="item-text"></div>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="http://ichangerapp.com/uriref.php?a=Passbook&p=pb-novus.png&n=Passbook&ur=shoebox://" class="external item-link item-content">
+						<div class="item-media">
+							<img src="http://ichangerapp.com/Passbook/pb-novus.png" width="80">
+						</div>
+						<div class="item-inner">
+							<div class="item-title-row">
+								<div class="item-title">Novus</div>
+								<div class="item-after">Install</div>
+							</div>
+							<div class="item-subtitle">FFra</div>
+							<div class="item-text"></div>
+						</div>
+					</a>
+				</li>
+		</div>
+	</div>
+</div>
