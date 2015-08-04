8af5ac608fd6f1054273e47187ab7f08e577a98c
Rosco1502 <thelocken@gmail.com>
2015-08-04 04:55:46 +0000

    Working on Profile System

--- 2/uriprofile.php
+++ 2/uriprofile.php
@@ -18,71 +18,70 @@ if ( $iconcdir == "custom" ) {
 } else {
     $iconfolder = ''.$icondir.'';
 }
-$u1 = rand(15, 15);
-$u2 = rand(4, 4);
-$u3 = rand(4, 4);
-$u4 = rand(12, 12);
 
-$UUID2 =  rand;
+$UUID = "6371432E-".$appname."-4111-AE46-C23EDF4ADE9B";
 
-$imageraw = 'http://ichangerapp.com/' . htmlspecialchars($iconfolder) . '/' . htmlspecialchars($iconmain) .'' . htmlspecialchars($iconext) . '';
-$image = file_get_contents('http://ichangerapp.com/' . htmlspecialchars($iconfolder) . '/' . htmlspecialchars($iconmain) .'' . htmlspecialchars($iconext) . '');
-$imageData = base64_encode($image);
+$path = 'http://ichangerapp.com/' . htmlspecialchars($iconfolder) . '/' . htmlspecialchars($iconmain) .'' . htmlspecialchars($iconext) . '';
+$type = pathinfo($path, PATHINFO_EXTENSION);
+$data = file_get_contents($path);
+$base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
+$base64raw = base64_encode($data);
 
 
-$mobileconfig .= '<?xml version="1.0" encoding="UTF-8"?>';
-$mobileconfig .= '<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">';
-$mobileconfig .= '<plist version="1.0">';
-$mobileconfig .= "<dict>";
-$mobileconfig .= "	<key>PayloadContent</key>";
-$mobileconfig .= "	<array>";
-$mobileconfig .= "		<dict>";
-$mobileconfig .= "			<key>FullScreen</key>";
-$mobileconfig .= "			<true/>";
-$mobileconfig .= "			<key>Icon</key>";
-$mobileconfig .= "			<data>".$imageData."</data>";
-$mobileconfig .= "			<key>IsRemovable</key>";
-$mobileconfig .= "			<true/>";
-$mobileconfig .= "			<key>Label</key>";
-$mobileconfig .= "			<string>".$appname."</string>";
-$mobileconfig .= "			<key>PayloadDescription</key>";
-$mobileconfig .= "			<string>Configures settings for a Web Clip</string>";
-$mobileconfig .= "			<key>PayloadDisplayName</key>";
-$mobileconfig .= "			<string>".$appname."</string>";
-$mobileconfig .= "			<key>PayloadIdentifier</key>";
-$mobileconfig .= "			<string>".$UUID2."</string>";
-$mobileconfig .= "			<key>PayloadType</key>";
-$mobileconfig .= "			<string>com.apple.webClip.managed</string>";
-$mobileconfig .= "			<key>PayloadUUID</key>";
-$mobileconfig .= "			<string>E8A00251-AD63-4C25-9242-A2327D3D88B2</string>";
-$mobileconfig .= "			<key>PayloadVersion</key>";
-$mobileconfig .= "			<real>1</real>";
-$mobileconfig .= "			<key>Precomposed</key>";
-$mobileconfig .= "			<true/>";
-$mobileconfig .= "			<key>URL</key>";
-$mobileconfig .= "			<string>".$appurl."</string>";
-$mobileconfig .= "		</dict>";
-$mobileconfig .= "	</array>";
-$mobileconfig .= "	<key>PayloadDescription</key>";
-$mobileconfig .= "	<string>iChanger Profile WIP: Shortcut for ".$appname.". This function is still a Work-In-Progress and is highly experimental if you find any problems please contact us from the menu in iChanger.</string>";
-$mobileconfig .= "	<key>PayloadDisplayName</key>";
-$mobileconfig .= "	<string>iChanger Profile for ".$appname."</string>";
-$mobileconfig .= "	<key>PayloadIdentifier</key>";
-$mobileconfig .= "	<string>com.thelocken.ichanger.'.$u1.'-'.$u2'-'.$u3'-'.$u4.'</string>";
-$mobileconfig .= "	<key>PayloadOrganization</key>";
-$mobileconfig .= "	<string>TheLocken LLC; iChanger</string>";
-$mobileconfig .= "	<key>PayloadRemovalDisallowed</key>";
-$mobileconfig .= "	<false/>";
-$mobileconfig .= "	<key>PayloadType</key>";
-$mobileconfig .= "	<string>Configuration</string>";
-$mobileconfig .= "	<key>PayloadUUID</key>";
-$mobileconfig .= "	<string>E8A00251-AD63-4C25-9242-A2327D3D88B2</string>";
-$mobileconfig .= "	<key>PayloadVersion</key>";
-$mobileconfig .= "	<integer>1</integer>";
-$mobileconfig .= "</dict>";
-$mobileconfig .= "</plist>";
+$mobileconfig = '<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>PayloadContent</key>
+	<array>
+		<dict>
+			<key>FullScreen</key>
+			<false/>
+			<key>Icon</key>
+			<data>'.$base64raw.'</data>
+			<key>IsRemovable</key>
+			<true/>
+			<key>Label</key>
+			<string>'.$appname.'</string>
+			<key>PayloadDescription</key>
+			<string>Configures settings for a Web Clip</string>
+			<key>PayloadDisplayName</key>
+			<string>'.$appname.'</string>
+			<key>PayloadIdentifier</key>
+			<string>com.thelocken.ichanger</string>
+			<key>PayloadType</key>
+			<string>com.apple.webClip.managed</string>
+			<key>PayloadUUID</key>
+			<string>'.$uuid.'</string>
+			<key>PayloadVersion</key>
+			<real>1</real>
+			<key>Precomposed</key>
+			<true/>
+			<key>URL</key>
+			<string>'.$appurl.'</string>
+		</dict>
+	</array>
+	<key>PayloadDescription</key>
+	<string>iChanger Profile WIP: Shortcut for '.$appname.'. This function is still a Work-In-Progress and is highly experimental if you find any problems please contact us from the menu in iChanger.</string>
+	<key>PayloadDisplayName</key>
+	<string>iChanger Profile for '.$appname.'</string>
+	<key>PayloadIdentifier</key>
+	<string>com.thelocken.ichanger.'.$uuid.'</string>
+	<key>PayloadOrganization</key>
+	<string>TheLocken LLC; iChanger</string>
+	<key>PayloadRemovalDisallowed</key>
+	<false/>
+	<key>PayloadType</key>
+	<string>Configuration</string>
+	<key>PayloadUUID</key>
+	<string>'.$uuid.'</string>
+	<key>PayloadVersion</key>
+	<integer>1</integer>
+</dict>
+</plist>';
 
-header('Content-type: application/x-apple-aspen-config; chatset=utf-8');
-header('Content-Disposition: attachment; filename="iChangerProfile.mobileconfig"');
+//header('Content-type: application/x-apple-aspen-config; charset=utf-8');
+header("Content-type: plain/text");
+header('Content-Disposition: attachment; filename="company.mobileconfig"');
 echo $mobileconfig;
 ?>
--- 2/uriref.php
+++ 2/uriref.php
@@ -8,7 +8,7 @@ $appurl = '' . htmlspecialchars($_GET["ur"]) . '';
 $v = '' . htmlspecialchars($_GET["v"]) . '';
 $gplus = '' . htmlspecialchars($_GET["gplus"]) . '';
 $switch = '' . htmlspecialchars($_GET["switch"]) . '';
-$url = "uridata.php";
+$url = "uriprofile.php";
 if ( $gplus == "yes" ) {
 	$gplusurl = "%2B";
 }
